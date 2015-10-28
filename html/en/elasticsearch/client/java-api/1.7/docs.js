bea0265228866d784457cbc6a6e9da2f41b68cf7
Clinton Gormley <clint@traveljury.com>
2015-10-28 16:21:39 +0100

    Updated docs

--- html/en/elasticsearch/client/java-api/1.7/docs.js
+++ html/en/elasticsearch/client/java-api/1.7/docs.js
@@ -2,6 +2,75 @@ jQuery(function() {
   // Move rtp container to top right and make visible
   jQuery('#rtpcontainer').prependTo('#guide').show();
 
+  var default_sense_url = 'http://localhost:5601/app/sense/';
+  var default_sense_url_marvel = 'http://localhost:9200/_plugin/marvel/sense/';
+  var sense_url = jQuery.cookie('sense_url') || default_sense_url;
+
+  // Enable Sense widget
+  init_sense_widgets(sense_url);
+
+  function init_sense_widgets(sense_url) {
+    var base_url = window.location.href.replace(/\/[^/?]+(?:\?.*)?$/, '/');
+    jQuery('div.sense_widget')
+      .each(
+        function() {
+          var div = jQuery(this);
+          var snippet = div.attr('data-snippet');
+          div
+            .html('<a class="sense_widget" target="sense" '
+              + 'title="Open snippet in Sense" '
+              + 'href="'
+              + sense_url
+              + '?load_from='
+              + base_url
+              + snippet
+              + '">View in Sense</a>'
+              + '<a class="sense_settings" title="Configure Sense URL">&nbsp;</a>');
+          div.find('a.sense_settings').click(sense_settings);
+        });
+  }
+
+  function sense_settings(e) {
+    e.stopPropagation();
+    if (jQuery('#sense_settings').length > 0) {
+      return;
+    }
+
+    var div = jQuery('<div id="sense_settings">'
+      + '<form>'
+      + '<label for="sense_url">Enter the URL of the Sense editor:</label>'
+      + '<input id="sense_url" type="text" value="'
+      + sense_url
+      + '" />'
+      + '<button id="save_url"    type="button">Save</button>'
+      + '<button id="reset_url"   type="button">Default Sense URL</button>'
+      + '<button id="reset_url_1" type="button">Default Sense v1 URL (Marvel)</button>'
+      + '<p>Or install <a href="https://www.elastic.co/guide/en/sense/current/installing.html">'
+      + 'the Sense 2 editor' + '</a>.</p>' + '</form></div>');
+    jQuery('body').prepend(div);
+
+    div.find('#save_url').click(function(e) {
+      var new_url = jQuery('#sense_url').val() || default_sense_url;
+      if (new_url === default_sense_url) {
+        jQuery.cookie('sense_url', '');
+      } else {
+        jQuery.cookie('sense_url', new_url);
+      }
+      sense_url = new_url;
+      init_sense_widgets(sense_url);
+      div.remove();
+      e.stopPropagation();
+    });
+    div.find('#reset_url').click(function(e) {
+      jQuery('#sense_url').val(default_sense_url);
+      e.stopPropagation();
+    });
+    div.find('#reset_url_1').click(function(e) {
+      jQuery('#sense_url').val(default_sense_url_marvel);
+      e.stopPropagation();
+    });
+  }
+
   function init_toc() {
 
     var title = jQuery('#book_title');
@@ -23,16 +92,16 @@ jQuery(function() {
     });
 
     // Make book title in toc collapsible
-    if (jQuery('.collapsible').length > 0 ) {
-        title.addClass('collapsible').click(function() {
-          if (title.hasClass('show')) {
-            title.removeClass('show');
-            title.parent().find('.show').removeClass('show');
-          } else {
-            title.addClass('show');
-            title.parent().find('.collapsible').addClass('show');
-          }
-        });
+    if (jQuery('.collapsible').length > 0) {
+      title.addClass('collapsible').click(function() {
+        if (title.hasClass('show')) {
+          title.removeClass('show');
+          title.parent().find('.show').removeClass('show');
+        } else {
+          title.addClass('show');
+          title.parent().find('.collapsible').addClass('show');
+        }
+      });
     }
 
     // Clicking links or the version selector shouldn't fold/expand
@@ -78,7 +147,8 @@ jQuery(function() {
         if (title_container.length > 0) {
           // Exclude page title
           if (0 < items++) {
-            title_container.find('a,.added,.coming,.deprecated,.experimental').remove();
+            title_container.find('a,.added,.coming,.deprecated,.experimental')
+              .remove();
             var text = title_container.html();
             ul.append('<li><a href="#' + this.id + '">' + text + '</a></li>');
           }
@@ -98,7 +168,8 @@ jQuery(function() {
 
   var div = jQuery('div.toc');
   // Fetch toc.html unless there is already a .toc on the page
-  if (div.length == 0  && jQuery('#guide').children('.article,.book').length == 0) {
+  if (div.length == 0
+    && jQuery('#guide').children('.article,.book').length == 0) {
     var url = location.href.replace(/[^\/]+$/, 'toc.html');
     var toc = jQuery.get(url, {}, function(data) {
       jQuery('.titlepage').first().after(data);
