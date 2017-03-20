d6a9bbe6e379927bb3e74608eda724cf7940d33b
Nik Everett <nik9000@gmail.com>
2017-03-20 14:47:50 -0400

    Add include-tagged macro (#151)
    This macro allows you to include a portion of a file like this:
    ```
    ["source","java",subs="attributes,callouts,macros"]
    --------------------------------------------------
    include-tagged::{doc-tests}/DeleteDocumentationIT.java[delete-request]
    --------------------------------------------------
    <1> Index name
    <2> Type
    <3> Document id
    ```
    
    Which can be used to extract a section of a file like this:
    ```
            // tag::delete-request
            DeleteRequest request = new DeleteRequest(
                "index",    // <1>
                "type",     // <2>
                "id");      // <3>
            // end::delete-request
    ```

---
+++ resources/extract-tagged.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Extract a tagged portion of a file.
+
+def extract_tagged(target, tag):
+    import re
+    start = re.compile('^(\s+).+tag::' + tag)
+    end = re.compile('end::' + tag)
+    foundTag = False
+
+    result = ''
+    with open(target, 'r') as f:
+        for line in f:
+            if end.search(line):
+                return result
+            if foundTag:
+                result = result + line.replace(indentation, '', 1)
+            else:
+                m = start.search(line)
+                if m:
+                    foundTag = True
+                    indentation = m.group(1)
+
+
+if __name__ == '__main__':
+    from sys import argv, stdout, stderr
+    try:
+        result = extract_tagged(argv[1], argv[2])
+        if result:
+            stdout.write(result)
+        else:
+            raise Exception("Couldn't find tag")
+    except Exception as e:
+      stderr.write("asciidoc: ERROR: Extracting tag `" + argv[2] + "` from file `" + argv[1]+ "`: " + e.__str__() + "\n")
+      exit(1)
