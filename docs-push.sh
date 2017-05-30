6f7581add8ca091a2ed16ec2ece1c2a334dc46c6
esdocs <esdocs@users.noreply.github.com>
2017-05-30 11:44:19 +0530

    Update docs-push.sh

--- docs-push.sh
+++ docs-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get latest code 
-cd /home/rawengwww/temp/docs && git pull
+cd /home/rawengwww/temp/docs && CURRENT="$(git symbolic-ref --short HEAD)" && git fetch && git reset --hard origin/$CURRENT
