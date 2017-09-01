7418a9b665da27fcb179910a9edaf176f30b21de
esdocs <esdocs@users.noreply.github.com>
2017-09-01 11:57:16 +0530

    Deployment script change

--- docs-push.sh
+++ docs-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get latest code 
-cd /home/rawengwww/temp/docs && CURRENT="$(git symbolic-ref --short HEAD)" && git fetch && git reset --hard origin/$CURRENT
+cd /opt/node_apps/docs && CURRENT="$(git symbolic-ref --short HEAD)" && git fetch && git reset --hard origin/$CURRENT
