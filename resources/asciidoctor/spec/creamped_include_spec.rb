a3ea7f03ea4bea19f0095b0e7897172456acf24a
Nik Everett <nik9000@gmail.com>
2019-01-08 09:55:57 -0500

    Asciidoctor: Work around "cramped" include statements
    We do a fair bit of:
    
    ```
    include::resources/1.adoc[]
    include::resources/2.adoc[]
    ```
    
    In our asciidoc files. Asciidoctor would prefer that we do:
    
    ```
    include::resources/1.adoc[]
    
    include::resources/2.adoc[]
    ```
    
    but asciidoc doesn't care. To be compatible with asciidoc this works
    around asciidoctor's preference, preventing the parsing error that come
    about because of using these "cramped" include statements by adding a
    trailing new line every time we include an asciidoc file. This is a
    little crude but it works like a charm.
    
    With this, I can now build the Elasticsearch Java REST Client reference
    without `--lenient` and the document looks great! This is how long the
    build takes with asciidoctor:
    
    ```
    real  0m7.652s
    user  0m7.080s
    sys   0m0.538s
    ```
    
    And this is how long it takes with asciidoc:
    
    ```
    real  0m44.987s
    user  0m38.715s
    sys   0m6.256s
    ```

---
+++ resources/asciidoctor/spec/creamped_include_spec.rb
@@ -0,0 +1,70 @@
+require 'added/extension'
+require 'cramped_include/extension'
+
+RSpec.describe CrampedInclude do
+  before(:each) do
+    Extensions.register do
+      preprocessor CrampedInclude
+    end
+  end
+
+  after(:each) do
+    Extensions.unregister_all
+  end
+
+  it "allows cramped includes of callout lists" do
+    actual = convert <<~ASCIIDOC
+      = Test
+
+      == Test
+
+      include::resources/cramped_include/colist1.adoc[]
+      include::resources/cramped_include/colist2.adoc[]
+    ASCIIDOC
+    expected = <<~DOCBOOK
+      <chapter id="_test">
+      <title>Test</title>
+      <section id="P1">
+      <title>P1</title>
+      <section id="P1_1">
+      <title>P1.1</title>
+      <programlisting language="java" linenumbering="unnumbered">words <1> <2></programlisting>
+      <calloutlist>
+      <callout arearefs="CO1-1">
+      <para>foo</para>
+      </callout>
+      </calloutlist>
+      </section>
+      </section>
+      <section id="P2">
+      <title>P2</title>
+      <section id="P2_1">
+      <title>P2.1</title>
+      <programlisting language="java" linenumbering="unnumbered">words <1> <2></programlisting>
+      <calloutlist>
+      <callout arearefs="CO2-1">
+      <para>foo</para>
+      </callout>
+      </calloutlist>
+      </section>
+      </section>
+      </chapter>
+    DOCBOOK
+    expect(actual).to eq(expected.strip)
+  end
+
+  it "doesn't break includes of non-asciidoc files" do
+    actual = convert <<~ASCIIDOC
+      ----
+      include::resources/cramped_include/Example.java[]
+      ----
+    ASCIIDOC
+    expected = <<~DOCBOOK
+      <preface>
+      <title></title>
+      <screen>public class Example {}</screen>
+      </preface>
+    DOCBOOK
+    expect(actual).to eq(expected.strip)
+  end
+end
