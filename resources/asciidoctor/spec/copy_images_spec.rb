feabefbf5605b4a7c34bc134af4cdc91c3c8df57
Nik Everett <nik9000@gmail.com>
2019-03-25 15:00:36 -0400

    NOCOMMITs

--- resources/asciidoctor/spec/copy_images_spec.rb
+++ resources/asciidoctor/spec/copy_images_spec.rb
@@ -46,11 +46,11 @@ RSpec.describe CopyImages do
     include_context 'convert'
     it 'copies the image' do
       expect(copied).to eq([
-          [image_uri, "#{spec_dir}/resources/copy_images/example1.png"],
+          [resolved, "#{spec_dir}/resources/copy_images/example1.png"],
       ])
     end
     it 'logs that it copied the image' do
-      expect(logs).to eq("INFO: <stdin>: line #{include_line}: copying #{spec_dir}\/resources\/copy_images\/example1.png")
+      expect(logs).to eq("INFO: <stdin>: line #{include_line}: copying #{spec_dir}/resources/copy_images/example1.png")
     end
   end
   shared_examples 'copies images' do
@@ -61,25 +61,24 @@ RSpec.describe CopyImages do
       ASCIIDOC
     end
     let(:include_line) { 2 }
-    # NOCOMMIT rename image_ref and image_uri. target and resolved?
     context 'when the image ref matches that path exactly' do
-      let(:image_ref) { 'resources/copy_images/example1.png' }
-      let(:image_uri) { 'resources/copy_images/example1.png' }
+      let(:target)   { 'resources/copy_images/example1.png' }
+      let(:resolved) { 'resources/copy_images/example1.png' }
       include_examples 'copies the image'
     end
     context 'when the image ref is just the name of the image' do
-      let(:image_ref) { 'example1.png' }
-      let(:image_uri) { 'example1.png' }
+      let(:target)   { 'example1.png' }
+      let(:resolved) { 'example1.png' }
       include_examples 'copies the image'
     end
     context 'when the image ref matches the end of the path' do
-      let(:image_ref) { 'copy_images/example1.png' }
-      let(:image_uri) { 'copy_images/example1.png' }
+      let(:target)   { 'copy_images/example1.png' }
+      let(:resolved) { 'copy_images/example1.png' }
       include_examples 'copies the image'
     end
     context 'when the image contains attributes' do
-      let(:image_ref) { 'example1.{ext}' }
-      let(:image_uri) { 'example1.png' }
+      let(:target)   { 'example1.{ext}' }
+      let(:resolved) { 'example1.png' }
       let(:input) do
         <<~ASCIIDOC
           == Example
@@ -94,20 +93,18 @@ RSpec.describe CopyImages do
   end
 
   context 'for the image block macro' do
-    let(:image_command) { "image::#{image_ref}[]" }
+    let(:image_command) { "image::#{target}[]" }
     include_examples 'copies images'
   end
   context 'for the image inline macro' do
-    # NOCMOMIT rename image_ref to target
-    # NOCOMMIT replace parameters with let
-    let(:image_command) { "Words image:#{image_ref}[] words" }
+    let(:image_command) { "Words image:#{target}[] words" }
     include_examples 'copies images'
     context 'when the macro is escaped' do
-      let(:image_ref) { 'example1.jpg' }
+      let(:target) { 'example1.jpg' }
       let(:input) do
         <<~ASCIIDOC
           == Example
-          "Words \\image:#{image_ref}[] words"
+          "Words \\image:#{target}[] words"
         ASCIIDOC
       end
       include_context 'convert'
@@ -118,7 +115,31 @@ RSpec.describe CopyImages do
         expect(copied).to eq([])
       end
     end
-    # NOCOMMIT multiple images in one line
+    context 'when there are multiple images on a line' do
+      let(:input) do
+        <<~ASCIIDOC
+          == Example
+
+          words image:example1.png[] words words image:example2.png[] words
+        ASCIIDOC
+      end
+      let(:expected_warnings) do
+        <<~WARNINGS
+          INFO: <stdin>: line 3: copying #{spec_dir}/resources/copy_images/example1.png
+          INFO: <stdin>: line 3: copying #{spec_dir}/resources/copy_images/example2.png
+        WARNINGS
+      end
+      include_context 'convert'
+      it 'copies the image' do
+        expect(copied).to eq([
+            ['example1.png', "#{spec_dir}/resources/copy_images/example1.png"],
+            ['example2.png', "#{spec_dir}/resources/copy_images/example2.png"],
+        ])
+      end
+      it 'logs that it copied the image' do
+        expect(logs).to eq(expected_warnings.strip)
+      end
+    end
   end
 
   it "warns when it can't find a file" do
