8fb39142df304806ec7c5537f743dbfe3e3cdd90
lcawl <lcawley@elastic.co>
2019-03-27 10:18:48 -0700

    Switches Kibana doc build to asciidoctor

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -21,7 +21,7 @@ alias docbldes=docbldesx
 alias docbldesold='$GIT_HOME/docs/build_docs --doc $GIT_HOME/elasticsearch/docs/reference/index.x.asciidoc --resource=$GIT_HOME/elasticsearch-extra/x-pack-elasticsearch/docs/ --chunk 1'
 
 # Kibana
-alias docbldkbx='$GIT_HOME/docs/build_docs --doc $GIT_HOME/kibana/docs/index.asciidoc --chunk 1'
+alias docbldkbx='$GIT_HOME/docs/build_docs --asciidoctor --doc $GIT_HOME/kibana/docs/index.asciidoc --chunk 1'
 
 alias docbldkb=docbldkbx
 
