50d71719404569c953fcbf73f7794e0f90e559eb
Nik Everett <nik9000@gmail.com>
2019-04-11 08:58:17 -0400

    integtest: Implement Asciidoctor dueling in rspec
    Implements a test that compares the output of converting the docs using
    Asciidoctor to using AsciiDoc and adds that to all "single book" test
    cases. This allows us to entirely move the tests that used a few files
    from the Makefile to rspec. Because we write those files to a tmpdir
    directly from the rspec file we can now remove the asciidoc files in the
    integtest folder.
    
    Also moves the test for the `experimental` admonition into rspec to be
    with its brother the `beta` admonition.

--- integtest/spec/helper/convert.rb
+++ integtest/spec/helper/convert.rb
@@ -6,9 +6,10 @@ require 'open3'
 # Method to convert asciidoc files to html in example blocks like `it`
 # and `before`.
 module Convert
-  def convert_single(from, to)
-    init_repo File.expand_path('..', from)
-    cmd = convert_single_cmd from, to
+  def convert_single(from, to, asciidoctor:)
+    from_dir = File.expand_path('..', from)
+    init_repo from_dir unless Dir.exist?("#{from_dir}/.git")
+    cmd = convert_single_cmd from, to, asciidoctor: asciidoctor
     # Use popen here instead of capture to keep stdin open to appease the
     # docker-image-always-removed paranoia in build_docs.pl
     _stdin, out, wait_thr = Open3.popen2e(*cmd)
@@ -21,12 +22,14 @@ module Convert
 
   private
 
-  def convert_single_cmd(from, to)
-    %W[
+  def convert_single_cmd(from, to, asciidoctor:)
+    opts = %W[
       /docs_build/build_docs.pl
       --in_standard_docker
       --doc #{from}
       --out #{to}
     ]
+    opts += ['--asciidoctor'] if asciidoctor
+    opts
   end
 end
