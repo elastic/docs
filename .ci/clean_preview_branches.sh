adfc0a467187c70953c699e8346f1033c8ec6ba7
Nik Everett <nik9000@gmail.com>
2019-06-04 16:52:49 -0400

    Create branch cleaner
    Creates a fairly simple script that removes the branches created by pull
    request builds when the pull requests have closed. The plan is to
    schedule this once a day or something in jenkins so it can keep the
    `build-docs` repo from growing too large.
    
    The docs preview service will notice the deleted branches and clean them
    up on the machines where it is running.

---
+++ .ci/clean_preview_branches.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# TODO pick up the image name automatically
+export REPO=git@github.com:elastic/built-docs.git
+export HOME=$JENKINS_HOME
+export IMAGE=docker.elastic.co/docs/build:1
+
+./build_docs --just-build-image
+ssh-agent bash -c "
+    ssh-add &&
+    docker run -it --rm \
+        -v$(pwd):/docs_build:cached,ro \
+        -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:cached,ro \
+        -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK:cached,ro \
+        -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+        -e GITHUB_TOKEN=$GITHUB_TOKEN \
+        -v ~/.git-references:/var/lib/jenkins/.git-references:cached,ro \
+        -e CACHE_DIR=/var/lib/jenkins/.git-references \
+        $IMAGE node /docs_build/preview/clean.js $REPO"
---
+++ preview/clean.js
@@ -0,0 +1,178 @@
+'use strict';
+
+const child_process = require('child_process');
+const dedent = require('dedent');
+const https = require('https');
+const os = require('os');
+const path = require('path');
+
+module.exports = Cleaner;
+module.exports.exec_git = exec_git;
+
+function Cleaner(token, repo, cache_dir, tmp_dir) {
+  this.repo = repo;
+  let repo_name = path.basename(repo);
+  if (!repo_name.endsWith('.git')) {
+    repo_name += '.git';
+  }
+  this.local_path = `${tmp_dir}/${repo_name}`;
+
+  this.run = () => {
+    return this.clone()
+      .then(this.show_heads)
+      .then(this.heads_to_prs)
+      .then(this.cleanup_closed_prs);
+  }
+
+  this.clone = () => {
+    return exec_git([
+      'clone',
+      '--bare',
+      '--reference', `${cache_dir}/${repo_name}`,
+      repo, this.local_path])
+  }
+
+  this.show_heads = () => {
+    return exec_git(['show-ref', '--heads'], {cwd: this.local_path});
+  }
+
+  this.heads_to_prs = (heads) => {
+    return heads
+      .split('\n')
+      .map(line => {
+        const found = line.match(/^.+ refs\/heads\/((.+)_(\d+))$/);
+        if (found) {
+          return {
+            branch: found[1],
+            repo: found[2],
+            number: Number(found[3]),
+          };
+        }
+      })
+      .filter(a => a)
+      .sort((lhs, rhs) => {
+        if (lhs.repo < rhs.repo) {
+          return -1;
+        }
+        if (lhs.repo > rhs.repo) {
+          return 1;
+        }
+        return lhs.number - rhs.number;
+      });
+  }
+
+  this.cleanup_closed_prs = async prs => {
+    for (let pr of prs) {
+      const url = `https://www.github.com/elastic/${pr.repo}/pull/${pr.number}`;
+      if (await this.is_pr_closed(pr)) {
+        console.info(`Deleting ${pr.branch} for closed pr at ${url}`);
+        if (pr.branch === 'master' || pr.branch === 'staging') {
+          // Just for super double ultra paranoia.
+          throw "Can't delete master!";
+        }
+        await exec_git(
+          ['push', 'origin', '--delete', pr.branch],
+          {cwd: this.local_path}
+        );
+      } else {
+        console.info(`Preserving ${pr.branch} for open pr at ${url}`);
+      }
+    }
+  }
+
+  this.is_pr_closed = function(pr) {
+    return new Promise((resolve, reject) => {
+      const body = {
+        query: `
+          query PRClosed($repo: String!, $number: Int!) {
+            repository(owner:\"elastic\", name:$repo) {
+              pullRequest(number:$number) {
+                closed
+              }
+            }
+          }
+        `,
+        variables: {
+          repo: pr.repo,
+          number: pr.number,
+        },
+      };
+      const postData = JSON.stringify(body);
+      const req = https.request({
+        method: 'POST',
+        host: 'api.github.com',
+        path: '/graphql',
+        headers: {
+          'Authorization': `bearer ${token}`,
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(postData),
+          'User-Agent': 'Elastic Docs Preview Cleaner Upper',
+        }
+      }, res => {
+        let data = '';
+        res.on('data', chunk => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          if (res.statusCode !== 200) {
+            reject(`Error getting ${JSON.stringify(pr)} [${res.statusCode}]:\n${data}`);
+          } else {
+            let closed;
+            try {
+              const parsed = JSON.parse(data);
+              closed = parsed.data.repository.pullRequest.closed;
+            } catch (e) {
+              reject(e);
+              return;
+            }
+            if (closed === undefined) {
+              reject(`unexpected reply from github:${data}`);
+              return;
+            }
+            if (res.headers['x-ratelimit-remaining'] < 100) {
+              const until = res.headers['x-ratelimit-reset'];
+              const millis = until * 1000 - Date.now().getTime();
+              console.info('rate limited for', millis, 'milliseconds');
+              setTimeout(() => resolve(closed), millis);
+            } else {
+              resolve(closed);
+            }
+          }
+        });
+      });
+      req.on('error', err => {
+        reject(`Error getting ${JSON.stringify(pr)}:\n${err}`);
+      });
+      req.write(postData);
+      req.end();
+    });
+  }
+}
+
+function exec_git(opts, env = {}) {
+  return new Promise((resolve, reject) => {
+    child_process.execFile('git', opts, env, (err, stdout, stderr) => {
+      if (err) {
+        reject(dedent `
+          err [${err}] running [git ${opts.join(' ')}] in ${JSON.stringify(env)}:
+          ${stderr}
+        `);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+if (require.main === module) {
+  const token = process.env['GITHUB_TOKEN']
+  const cache_dir = process.env['CACHE_DIR'];
+  const repo = process.argv[2];
+
+  process.on('SIGINT', function() {
+    process.exit(1);
+  });
+
+  new Cleaner(token, repo, cache_dir, os.tmpdir()).run()
+    .catch(err => console.error(err));
+}
