96b66ea93d7af0bf652552a55fae00cf40d66c48
Nik Everett <nik9000@gmail.com>
2019-07-31 19:46:41 -0400

    WIP

--- integtest/spec/helper/console_alternative_examples.rb
+++ integtest/spec/helper/console_alternative_examples.rb
@@ -20,7 +20,7 @@ RSpec.shared_examples 'README-like console alternatives' do |path|
     it 'contains the js example' do
       expect(body).to include(<<~HTML.strip)
         <div class="pre_wrapper alternative lang-js"><pre class="alternative programlisting prettyprint lang-js">const result = await client.search({
-          body: { query: 'foo bar' } <a id="js-8a7e0a79b1743d5fd94d79a7106ee930-CO1-1"></a><span><img src="images/icons/callouts/1.png" alt="" /></span>
+          body: { query: 'foo bar' } <a id="A0-CO1-1"></a><span><img src="images/icons/callouts/1.png" alt="" /></span>
         })</pre></div><div class="alternative lang-js calloutlist">
       HTML
       # The last line is important: we need the snippet to be followed
@@ -31,25 +31,47 @@ RSpec.shared_examples 'README-like console alternatives' do |path|
         <div class="pre_wrapper alternative lang-csharp"><pre class="alternative programlisting prettyprint lang-csharp">var searchResponse = _client.Search&lt;Project&gt;(s =&gt; s
             .Query(q =&gt; q
                 .QueryString(m =&gt; m
-                    .Query("foo bar") <a id="csharp-8a7e0a79b1743d5fd94d79a7106ee930-CO1-1"></a><span><img src="images/icons/callouts/1.png" alt="" /></span>
+                    .Query("foo bar") <a id="A1-CO1-1"></a><span><img src="images/icons/callouts/1.png" alt="" /></span>
                 )
             )
         );</pre></div><div class="alternative lang-csharp calloutlist">
       HTML
     end
   end
-  file_context "#{path}/missing_alternatives/console/js" do
-    it 'contains only the missing example' do
-      expect(contents).to eq(<<~LOG)
-        * d21765565081685a36dfc4af89e7cece.adoc: index.asciidoc: line 15
-      LOG
+  file_context "#{path}/alternatives_report.adoc" do
+    it 'has a report on the example with all alternatives' do
+      expect(contents).to include(<<~ASCIIDOC)
+        === index.asciidoc: line 6: 8a7e0a79b1743d5fd94d79a7106ee930
+        [source,console]
+        ----
+        GET /_search
+        {
+            "query": "foo bar" \\<1>
+        }
+        ----
+        |===
+        | js | csharp
+
+        | &check; | &check;
+        |===
+      ASCIIDOC
     end
-  end
-  file_context "#{path}/missing_alternatives/console/csharp" do
-    it 'contains only the missing example' do
-      expect(contents).to eq(<<~LOG)
-        * d21765565081685a36dfc4af89e7cece.adoc: index.asciidoc: line 15
-      LOG
+    it 'has a report on the example without any alternatives' do
+      expect(contents).to include(<<~ASCIIDOC)
+        === index.asciidoc: line 15: d21765565081685a36dfc4af89e7cece
+        [source,console]
+        ----
+        GET /_search
+        {
+            "query": "missing"
+        }
+        ----
+        |===
+        | js | csharp
+
+        | &cross; | &cross;
+        |===
+      ASCIIDOC
     end
   end
 end
