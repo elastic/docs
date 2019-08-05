91e7b7de3e7fa648fa7af9b2e5759c1bba9557fa
Nik Everett <nik9000@gmail.com>
2019-08-05 18:48:10 -0400

    Rubocop

--- resources/asciidoctor/lib/alternative_language_lookup/extension.rb
+++ resources/asciidoctor/lib/alternative_language_lookup/extension.rb
@@ -91,7 +91,7 @@ module AlternativeLanguageLookup
           next unless ['.asciidoc', '.adoc'].include? extension
 
           basename = File.basename path, extension
-          index[basename] = {path: path}
+          index[basename] = { path: path }
         end
       end
 
