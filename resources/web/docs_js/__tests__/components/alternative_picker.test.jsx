48109e72aca24937fdd7f4e55d34cf6478d141c9
Nik Everett <nik9000@gmail.com>
2019-08-23 15:59:02 -0400

    Prevent jumping when you pick an alternative lang (#1095)
    When you pick an alternative language the page has to reflow because the
    alternatives have different vertical size. This reflow looks like a big
    "jump", especially when you pick the language lower down on the page.
    This prevents the "jump" by storing the location of the picker before
    the reflow, updating the styles to cause the reflow, grabbing the new
    location, and then scrolling the window the difference between the new
    and old location. This keeps the picker, which is usually right under
    your mouse when you change the language, from jumping. This "feels"
    smooth!

--- resources/web/docs_js/__tests__/components/alternative_picker.test.jsx
+++ resources/web/docs_js/__tests__/components/alternative_picker.test.jsx
@@ -86,7 +86,10 @@ describe(_AlternativePicker, () => {
       * have to do it ourselves. Like an animal. */
     select.dispatchEvent(new Event("change"));
     test("saves the config", () => {
-      expect(updates).toStrictEqual([{consoleAlternative: 'js'}]);
+      // We can't use toMatchObject here because tagName is busted.
+      expect(updates).toHaveLength(1);
+      expect(updates[0].consoleAlternative).toBe("js");
+      expect(updates[0].alternativeChangeSource.tagName).toBe("SELECT");
     })
   });
 });
