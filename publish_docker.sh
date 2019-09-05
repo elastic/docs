47ad22ddc4d713054b1a4a7d9b442613cf5d2d1d
Nik Everett <nik9000@gmail.com>
2019-09-05 17:17:28 -0400

    Prepare to release a new version of preview
    We'd like to pick up the "go away" robots.txt.

--- publish_docker.sh
+++ publish_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 export BUILD=docker.elastic.co/docs/build:1
-export PREVIEW=docker.elastic.co/docs/preview:3
+export PREVIEW=docker.elastic.co/docs/preview:4
 
 cd $(git rev-parse --show-toplevel)
 ./build_docs --just-build-image
