843da32fe4119542410388cc2847b0743eb8c33b
Nik Everett <nik9000@gmail.com>
2019-09-09 15:05:08 -0400

    Streaming templates
    Replaces the template implementation copied from perl with a work-alike
    that uses async iterators and async generators. It has *slightly* higher
    overhead but streams the template and raw html rather than pulling it
    all into memory which protects it against very large html which should
    make it embeddable into the preview application so we can template on
    the fly.
    
    Note: to get this I had to update Node to 12.x to support turning an
    async generator into a readable stream. This is important because these
    streams properly handle backpressure. I don't really expect any
    backpressure when streaming to local files but I *do* expect it in the
    preview app.

--- template/__test__/template.test.js
+++ template/__test__/template.test.js
@@ -20,12 +20,12 @@
 
 'use strict';
 
-const template = require('../template');
+const Template = require('../template');
 
-describe(template, () => {
+describe(Template, () => {
   describe("buildInitialJsState", () => {
     describe("for empty alternatives summary", () => {
-      const state = template.buildInitialJsState({});
+      const state = Template.buildInitialJsState({});
       test("not to have any alternatives", () => {
         expect(state).toStrictEqual({
           alternatives: {}
@@ -33,7 +33,7 @@ describe(template, () => {
       });
     });
     describe("for a small alternatives summary", () => {
-      const state = template.buildInitialJsState({
+      const state = Template.buildInitialJsState({
         console: {
           total: 2,
           alternatives: {
@@ -65,14 +65,24 @@ describe(template, () => {
     });
   });
   describe("apply", () => {
-    let t;
-    beforeAll(async () => {
-      t = await template("../resources/web/template.html");
-    });
+    let template = Template("../resources/web/template.html")
+    template.applyToString = async (raw, lang, initialJsState) => {
+      const rawItr = (function* () {
+        yield raw;
+      })();
+      return template.applyToItr(rawItr, lang, initialJsState);
+    };
+    template.applyToItr = async (rawItr, lang, initialJsState) => {
+      let result = '';
+      for await (const chunk of template.apply(rawItr, lang, initialJsState)) {
+        result += chunk;
+      }
+      return result;
+    };
     describe("when applied to simple html", () => {
       let result;
-      beforeAll(() => {
-        result = t.apply(`
+      beforeAll(async () => {
+        result = await template.applyToString(`
           <!DOCTYPE html>
           <html>
             <head><title>foo</title></head>
@@ -96,16 +106,101 @@ describe(template, () => {
         );
       });
     });
+    describe("when the head and body are empty", () => {
+      let result;
+      beforeAll(async () => {
+        result = await template.applyToString(`
+          <!DOCTYPE html>
+          <html>
+            <head></head>
+            <body></body>
+          </html>`);
+      });
+      test("doesn't add anything to the head", () => {
+        expect(result).toMatch(
+          /<head>\s+<meta http-equiv="content-type"/
+        );
+      });
+      test("outputs an empty body", () => {
+        expect(result).toMatch(
+          /<!-- start body -->\s+<!-- end body -->/
+        );
+      });
+    });
     describe("when it gets a document without a head", () => {
-      test("throws an exception", () => {
-        expect(() => t.apply(`<html><body>words</body></html>`))
-          .toThrow(/Couldn't find head/);
+      test("throws an exception", async () => {
+        return expect(template.applyToString(`<html><body>words</body></html>`))
+          .rejects.toThrow(/Couldn't find <head> in raw/);
+      });
+    });
+    describe("when it gets a document with an unclosed head", () => {
+      test("throws an exception", async () => {
+        return expect(template.applyToString(`<html><head><body>words</body></html>`))
+          .rejects.toThrow(/Couldn't find <\/head> in raw/);
       });
     });
     describe("when it gets a document without a body", () => {
       test("throws an exception", () => {
-        expect(() => t.apply(`<html><head><script>foo</script></head></html>`))
-          .toThrow(/Couldn't find body/);
+        expect(template.applyToString(`<html><head><script>foo</script></head></html>`))
+          .rejects.toThrow(/Couldn't find <body> in raw/);
+      });
+    });
+    describe("when it gets a document with an unclosed body", () => {
+      test("throws an exception", () => {
+        expect(template.applyToString(`<html><head><script>foo</script></head><body></html>`))
+          .rejects.toThrow(/Couldn't find <\/body> in raw/);
+      });
+    });
+    describe("when the head start tag is in two chunks", () => {
+      let result;
+      beforeAll(async () => {
+        result = await template.applyToItr((function* () {
+          yield '<html><he';
+          yield 'ad>head stuff</head><body></body></html>';
+        })());
+      });
+      test("outputs the correct head anyway", () => {
+        expect(result).toMatch(/<head>\s+head stuff/);
+      });
+    });
+    describe("when the head e nd tag is in two chunks", () => {
+      let result;
+      beforeAll(async () => {
+        result = await template.applyToItr((function* () {
+          yield '<html><head>head stuff</he';
+          yield 'ad><body></body></html>';
+        })());
+      });
+      test("outputs the correct head anyway", () => {
+        expect(result).toMatch(/<head>\s+head stuff/);
+      });
+    });
+    describe("when the body start tag is in two chunks", () => {
+      let result;
+      beforeAll(async () => {
+        result = await template.applyToItr((function* () {
+          yield '<html><head></head><b';
+          yield 'ody>body stuff</body></html>';
+        })());
+      });
+      test("outputs the correct body anyway", () => {
+        expect(result).toMatch(
+          /<!-- start body -->\s+body stuff\s+<!-- end body -->/
+        );
+      });
+    });
+    describe("when the body end tag is in two chunks", () => {
+      let result;
+      beforeAll(async () => {
+        result = await template.applyToItr((function* () {
+          yield '<html><head></head><body>body stuff</';
+          yield 'body></html>';
+        })());
+      });
+      test("outputs the correct body anyway", () => {
+        expect(result).toMatch(
+          /<!-- start body -->\s+body stuff\s+<!-- end body -->/
+        );
       });
     });
   });
