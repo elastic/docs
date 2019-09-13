869b399a7e0de544e892cfa342ec1d8ad7bf4928
Josh Mock <joshua.mock@elastic.co>
2019-09-13 13:27:05 -0500

    Add tooltip when hovering over alt picker warning icon (#1188)
    * Add tooltip when hovering over alt picker warning icon
    
    * Export intermediate component for unit testing purposes
    
    * Make tests pass when tooltip is not in DOM but warning icon is

--- resources/web/docs_js/components/alternative_picker.jsx
+++ resources/web/docs_js/components/alternative_picker.jsx
@@ -3,7 +3,7 @@
  * instead of "Console".
  */
 
-import {h} from "../../../../../../node_modules/preact";
+import {h, Component} from "../../../../../../node_modules/preact";
 import {pick, merge} from "../../../../../node_modules/ramda";
 import {connect} from "../../../../../node_modules/preact-redux";
 import {saveSettings} from "../actions/settings";
@@ -21,50 +21,73 @@ const AlternativeChoice = ({name: name}) => {
   return <option value={name}>{alternativePrettyName(name)}</option>;
 };
 
-export const _AlternativePicker = ({
-  alternatives: alternatives,
-  consoleAlternative: consoleAlternative,
-  saveSettings: saveSettings,
-}) => {
-  if (!alternatives) {
-    return <div/>; // Empty div to preserve the spacing
-  }
-  const consoleAlternatives = alternatives.console;
-  if (!consoleAlternatives) {
-    return <div/>;
+export class _AlternativePicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tooltipVisible: false };
+    this.toggleWarning = this.toggleWarning.bind(this);
   }
 
-  const items = [];
-  let sawChoice = 'console' === consoleAlternative;
-  items.push(<AlternativeChoice name='console'/>);
-  for (const name of Object.keys(consoleAlternatives)) {
-    sawChoice |= name === consoleAlternative;
-    items.push(<AlternativeChoice name={name} />);
+  toggleWarning(show) {
+    this.setState({ tooltipVisible: show });
   }
 
-  /* If value isn't in the list then *make* it and we'll render our standard
-   * "there no example for this language" option. This prevents us from
-   * squashing preferences that users set. */
-  if (!sawChoice) {
-    items.push(<AlternativeChoice name={consoleAlternative} />);
-  }
-  // TODO we shouldn't change these drop downs after the first time they are rendered. The extra choice should stay while you stay on the page. Maybe we can get away with rendering this once on page load and never subscribing again?
+  render() {
+    const { alternatives, consoleAlternative, saveSettings } = this.props;
 
-  // TODO add the "message" bubble to the warning.
-  return <div className="AlternativePicker u-space-between">
-    <select className="AlternativePicker-select"
-            value={consoleAlternative}
-            onChange={(e) => {
-              saveSettings({
-                consoleAlternative: e.target.value,
-                alternativeChangeSource: e.target,
-              });
-            }}>
-      {items}
-    </select>
-    <div className="AlternativePicker-warning" />
-  </div>;
-};
+    if (!alternatives) {
+      return <div />;
+    }
+
+    const consoleAlternatives = alternatives.console;
+    if (!consoleAlternatives) {
+      return <div/>;
+    }
+
+    const items = [];
+    let sawChoice = 'console' === consoleAlternative;
+    items.push(<AlternativeChoice name='console'/>);
+    for (const name of Object.keys(consoleAlternatives)) {
+      sawChoice |= name === consoleAlternative;
+      items.push(<AlternativeChoice name={name} />);
+    }
+
+    /* If value isn't in the list then *make* it and we'll render our standard
+     * "there no example for this language" option. This prevents us from
+     * squashing preferences that users set. */
+    if (!sawChoice) {
+      items.push(<AlternativeChoice name={consoleAlternative} />);
+    }
+    // TODO we shouldn't change these drop downs after the first time they are rendered. The extra choice should stay while you stay on the page. Maybe we can get away with rendering this once on page load and never subscribing again?
+
+    return <div className="AlternativePicker u-space-between">
+      <select className="AlternativePicker-select"
+              value={consoleAlternative}
+              onChange={(e) => {
+                saveSettings({
+                  consoleAlternative: e.target.value,
+                  alternativeChangeSource: e.target,
+                });
+              }}>
+        {items}
+      </select>
+      <div
+        className="AlternativePicker-warning"
+        onMouseOver={() => this.toggleWarning(true)}
+        onMouseOut={() => this.toggleWarning(false)} />
+
+      { this.state.tooltipVisible
+          ? (
+            <div className="AlternativePicker-tooltip-wrapper">
+              <div className="AlternativePicker-tooltip" role="tooltip">
+                <div className="arrow"></div>
+                <div>No {alternativePrettyName(consoleAlternative)} version available for this example</div>
+              </div>
+            </div>
+          ) : null }
+    </div>;
+  }
+}
 
 export default connect(
   state => merge(
