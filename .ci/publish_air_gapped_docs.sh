0f4b4de16dde1289017a714be4bb1e3570122e9f
Nik Everett <nik9000@gmail.com>
2019-09-26 09:07:16 -0400

    Push air gapped docker image
    Modifies the ci script for the air gapped docker image to push it.

--- .ci/publish_air_gapped_docs.sh
+++ .ci/publish_air_gapped_docs.sh
@@ -2,5 +2,7 @@
 
 set -eo pipefail
 
+export AIR_GAPPED=docker.elastic.co/docs-private/air_gapped:latest
 ./air_gapped/build.sh
-# TODO publish
+docker tag $AIR_GAPPED push.$AIR_GAPPED
+docker push push.$AIR_GAPPED
