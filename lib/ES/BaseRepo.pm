5e5cb43b7dd13e4f309a95e811775ffe7af55ed1
Nik Everett <nik9000@gmail.com>
2019-10-03 16:45:29 -0400

    NOCOMMIT!

--- lib/ES/BaseRepo.pm
+++ lib/ES/BaseRepo.pm
@@ -66,7 +66,6 @@ sub sha_for_branch {
     local $ENV{GIT_DIR} = $self->git_dir;
     $branch = $self->normalize_branch( $branch );
     my $sha = eval { run 'git', 'rev-parse', $branch } || ''; # NOCOMMIT check if eval is still useful
-    chomp $sha; # NOCOMMIT check if we still need to chomp
     return $sha;
 }
 
