a56ed0d6f7f9caf20e5a528bf9bfddfaccd1a0d7
Nik Everett <nik9000@gmail.com>
2019-10-22 13:01:25 -0400

    Move the air gapped docs
    It turns out we do want them publicly accessible.

--- .ci/publish_air_gapped_docs.sh
+++ .ci/publish_air_gapped_docs.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-export AIR_GAPPED=docker.elastic.co/docs-private/air_gapped:latest
+export AIR_GAPPED=docker.elastic.co/docs/air_gapped:latest
 ./air_gapped/build.sh
 docker tag $AIR_GAPPED push.$AIR_GAPPED
 docker push push.$AIR_GAPPED
