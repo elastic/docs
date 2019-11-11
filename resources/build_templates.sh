831eae4cad3c1edfd867075ec8d9fedf4e074cdb
Nik Everett <nik9000@gmail.com>
2019-11-11 08:16:29 -0500

    Drop some more asciidoc leftovers (#1395)
    AsciiDoc is gone!

--- resources/build_templates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname $0)
-
-FILE="fo_titlepage"
-echo "Generating $FILE.xsl"
-xsltproc --output $BASEDIR/$FILE.xsl $BASEDIR/docbook-xsl-1.78.1/template/titlepage.xsl $BASEDIR/${FILE}_template.xml
