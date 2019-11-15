9553aa25f1428ea932012937c2d677f8cabd1def
Nik Everett <nik9000@gmail.com>
2019-11-15 15:49:36 -0500

    Speed up tests
    Builds the testing docker images during the packer cached step of the
    process that builds the images that run the docs build. So *usually*
    we'll be able to use the cached copy. Which speeds up the tests.
    
    As a bonus this builds the html diff tool during the tests and packer
    cache phase. This both makes sure that we have the image if we need it
    *and* makes sure that we don't break being about to build the image when
    we make changes.

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+# It'd be a real shame if we couldn't build the packer cache the morning after
+# we merge. So let's test that. We'll use the images that it builds to run the
+# rests of the tests anyway.
+echo "Building packer cache"
+./.ci/packer_cache.sh
+
 # Configure the git author and committer information. The tests expect there
 # to be *something* set ut don't care what it is.
 export GIT_AUTHOR_NAME='Jenkins CI'
