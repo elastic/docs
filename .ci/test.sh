2267d819ea1baa0fc3e1112fa8d9594db84af1b9
Nik Everett <nik9000@gmail.com>
2019-11-18 13:30:24 -0500

    Revert accident

--- .ci/test.sh
+++ .ci/test.sh
@@ -9,12 +9,5 @@ set -eo pipefail
 echo "Building packer cache"
 ./.ci/packer_cache.sh
 
-# Configure the git author and committer information. The tests expect there
-# to be *something* set ut don't care what it is.
-export GIT_AUTHOR_NAME='Jenkins CI'
-export GIT_AUTHOR_EMAIL='jenkins@elasticsearch-ci.elastic.co'
-export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
-export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
-
 cd $(git rev-parse --show-toplevel)
 make
