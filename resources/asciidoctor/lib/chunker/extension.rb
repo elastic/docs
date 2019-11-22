32bb23a9f7d87813b985841d8b8eecebdfe815f3
Nik Everett <nik9000@gmail.com>
2019-11-22 16:47:53 -0500

    Fix the title on chunked pages
    In docbook we set the title to chunked pages to
    "Chunk Title | Main Title | Elastic" so this does the same in
    direct_html.

--- resources/asciidoctor/lib/chunker/extension.rb
+++ resources/asciidoctor/lib/chunker/extension.rb
@@ -29,7 +29,7 @@ module Chunker
       chunk_level = doc.attr 'chunk_level'
       return yield unless node.level == chunk_level
 
-      html = form_section_into_page doc, yield
+      html = form_section_into_page doc, node.title, yield
       target = write doc, node.id, html
       link_opts = { type: :link, target: target }
       doc.register :links, target
@@ -37,11 +37,13 @@ module Chunker
       %(<li><span class="chapter">#{link.convert}</span></li>)
     end
 
-    def form_section_into_page(doc, html)
+    def form_section_into_page(doc, title, html)
       # We don't use asciidoctor's "parent" documents here because they don't
       # seem to buy us much and they are an "internal" detail.
       subdoc = Asciidoctor::Document.new [], subdoc_opts(doc)
       subdoc << Asciidoctor::Block.new(subdoc, :pass, source: html)
+      maintitle = doc.doctitle partition: true
+      subdoc.attributes['title'] = "#{title} | #{maintitle.main}"
       subdoc.convert
     end
 
