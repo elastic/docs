c39cbcf3fdcac60e95846a23e3ccae957238b330
Nik Everett <nik9000@gmail.com>
2019-11-27 08:59:51 -0500

    Render deep toc in --direct_html
    This enables rendering subsections in the table of contents in a way
    that mimicks docbook. In doing so it fixes a crash when trying to
    convert chunked books that have subsections caused by the chunker not
    seeing the subsections in the toc. This adds them so it is happy.

--- resources/asciidoctor/lib/docbook_compat/convert_outline.rb
+++ resources/asciidoctor/lib/docbook_compat/convert_outline.rb
@@ -4,19 +4,28 @@ module DocbookCompat
   ##
   # Methods to convert the table of contents.
   module ConvertOutline
-    def convert_outline(node)
+    def convert_outline(node, opts = {})
       # Asciidoctor's implementation looks lovely but doesn't match docbook's
       # implementation. So we drop our own in. We should see if we can use
       # Asciidoctor's implementation ASAP though.
-      result = [%(<ul class="toc">\n)]
-      node.sections.each do |section|
-        result << <<~HTML
-          <li><span class="#{wrapper_class_for section}"><a href="##{section.id}">#{section.title}</a></span>
-          </li>
-        HTML
-      end
+      toclevels = opts[:toclevels] || node.document.attributes['toclevels'].to_i
+      result = [%(<ul class="toc">)]
+      result += node.sections.map { |s| outline_section s, toclevels }
       result << '</ul>'
-      result.join
+      result.join "\n"
+    end
+
+    def outline_section(section, toclevels)
+      link = %(<a href="##{section.id}">#{section.title}</a>)
+      link = %(<span class="#{wrapper_class_for section}">#{link}</span>)
+      result = [%(<li>#{link})]
+      if section.level < toclevels && section.sections?
+        result << '<ul>'
+        result += section.sections.map { |s| outline_section s, toclevels }
+        result << '</ul>'
+      end
+      result << '</li>'
+      result
     end
   end
 end
