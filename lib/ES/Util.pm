7695f91c03527581c06d2086f28be48ebc0e5224
Nik Everett <nik9000@gmail.com>
2019-12-02 11:46:43 -0500

    Add version to some titles in direct_html (#1491)
    Docbook adds the version of the book to some title attributes on some
    links. This teaches `--direct_html` to add them there too.

--- lib/ES/Util.pm
+++ lib/ES/Util.pm
@@ -120,6 +120,7 @@ sub build_chunked {
                 '-a' => 'dc.type=Learn/Docs/' . $section,
                 '-a' => 'dc.subject=' . $subject,
                 '-a' => 'dc.identifier=' . $version,
+                $multi ? ( '-a' => "title-extra= [$version]" ) : (),
                 $noindex ? ('-a' => 'noindex') : (),
                 $page_header ? ('-a' => "page-header=$page_header") : (),
             ) : (),
@@ -281,6 +282,7 @@ sub build_single {
                 '-a' => 'dc.type=Learn/Docs/' . $section,
                 '-a' => 'dc.subject=' . $subject,
                 '-a' => 'dc.identifier=' . $version,
+                $multi ? ( '-a' => "title-extra= [$version]" ) : (),
                 $noindex ? ('-a' => 'noindex') : (),
                 $page_header ? ('-a' => "page-header=$page_header") : (),
                 # Turn on asciidoctor's table of contents generation if we want a TOC
