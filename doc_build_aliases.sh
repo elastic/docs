f41778b54193fd1a61c5285d1d2b88ea41bc4ce6
Nik Everett <nik9000@gmail.com>
2019-12-04 08:32:31 -0500

    Switch azure docs to direct_html (#1506)
    direct_html is faster and more customizable. It is the future.

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -58,7 +58,7 @@ alias docbldso='$GIT_HOME/docs/build_docs --doc $GIT_HOME/stack-docs/docs/en/sta
 alias docbldsoold=docbldso
 
 # Deploying Azure
-alias docbldaz='$GIT_HOME/docs/build_docs --asciidodctor --doc $GIT_HOME/azure-marketplace/docs/index.asciidoc --chunk 1'
+alias docbldaz='$GIT_HOME/docs/build_docs --direct_html --doc $GIT_HOME/azure-marketplace/docs/index.asciidoc --chunk 1'
 
 # Solutions
 alias docbldmet='$GIT_HOME/docs/build_docs --doc $GIT_HOME/stack-docs/docs/en/metrics/index.asciidoc --chunk 1'
