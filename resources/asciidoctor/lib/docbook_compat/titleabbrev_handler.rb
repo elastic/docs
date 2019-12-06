f63536198c8b5d42f3363e1c7cdd0258aff78d05
Nik Everett <nik9000@gmail.com>
2019-12-06 14:22:00 -0500

    Rework titleabbrev handling in direct_html
    This reworks `<titleabbrev>` handling in direct_html so it works for
    link targets as well. It turns out asciidoctor has a built in thing for
    this called `reftext`. So we just use it.

--- resources/asciidoctor/lib/docbook_compat/titleabbrev_handler.rb
+++ resources/asciidoctor/lib/docbook_compat/titleabbrev_handler.rb
@@ -7,7 +7,7 @@ module DocbookCompat
   # Looks for pass blocks with `<titleabbrev>` and adds an attributes to their
   # parent section. This attribute is an abbreviated title used when rendering
   # the table of contents. This exists entirely for backwards compatibility with
-  # docbook. It is simpler and recommended to set the `titleabbrev` attribute
+  # docbook. It is simpler and recommended to set the `reftext` attribute
   # directly on the section when the document is built with `--direct_html`.
   class TitleabbrevHandler < TreeProcessorScaffold
     def process_block(block)
@@ -22,10 +22,17 @@ module DocbookCompat
 
       text.slice! m.begin(0), m.end(0)
       block.lines = text.split "\n"
+      process_titleabbrev block, m[1]
+    end
 
+    private
+
+    def process_titleabbrev(block, reftext)
       section = block.parent
       section = section.parent until section.context == :section
-      section.attributes['titleabbrev'] = m[1]
+      # Docbook seems to bold links to sections less than 2 so we should too.
+      reftext = "_#{reftext}_" if section.level < 2
+      section.attributes['reftext'] = reftext
     end
   end
 end
