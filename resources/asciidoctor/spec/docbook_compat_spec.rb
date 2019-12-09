00b6597890826f158641d4b13e5807fe8e298aa6
Nik Everett <nik9000@gmail.com>
2019-12-09 14:21:02 -0500

    Handle missing titles in direct_html
    When you link to a section that doesn't have a title at all we crashed!
    Crash bad! No crash! This skips the `title` tag if the section we link
    to doesn't have a title.

--- resources/asciidoctor/spec/docbook_compat_spec.rb
+++ resources/asciidoctor/spec/docbook_compat_spec.rb
@@ -840,6 +840,19 @@ RSpec.describe DocbookCompat do
         expect(converted).to include('title="foo"')
       end
     end
+    context 'when the section title is empty' do
+      let(:input) do
+        <<~ASCIIDOC
+          Words <<foo>>.
+
+          [[foo]]
+          ==
+        ASCIIDOC
+      end
+      it "contains the target's title without the markup" do
+        expect(converted).not_to include('title')
+      end
+    end
     context 'when the cross reference is to an inline anchor' do
       let(:input) do
         <<~ASCIIDOC
