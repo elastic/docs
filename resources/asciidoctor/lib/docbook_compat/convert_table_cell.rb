83ae3a96ce968558ff32bd5daec9d5da4d96a7b2
Nik Everett <nik9000@gmail.com>
2019-12-10 10:57:05 -0500

    Support more table formatting in direct_html
    Asciidoctor supports explicitly setting the format on table cell so we
    should respect that in direct_html. This sets up direct_html to mimic
    how it all turns out in docbook.

--- resources/asciidoctor/lib/docbook_compat/convert_table_cell.rb
+++ resources/asciidoctor/lib/docbook_compat/convert_table_cell.rb
@@ -2,17 +2,11 @@
 
 module DocbookCompat
   ##
-  # Methods to convert tables.
+  # Methods to convert table cells.
   module ConvertTableCell
-    def convert_table_cell(cell, data_tag, wrap_text)
+    def convert_table_cell(cell, data_tag, allow_formatting)
       result = [convert_cell_open(cell, data_tag)]
-      if cell.inner_document
-        result << "\n" << cell.content << "\n"
-      else
-        result << '<p>' if wrap_text
-        result << cell.text
-        result << '</p>' if wrap_text
-      end
+      result += convert_cell_content(cell, allow_formatting)
       result << '</' << data_tag << '>'
       result.join
     end
@@ -29,11 +23,66 @@ module DocbookCompat
 
     def cell_open_attrs(cell)
       {
-        align: 'left',
+        align: cell.attr('halign'),
         colspan: cell.colspan == 1 ? nil : cell.colspan,
         rowspan: cell.rowspan == 1 ? nil : cell.rowspan,
-        valign: 'top',
+        valign: cell.attr('valign'),
       }.compact
+    end
+
+    def convert_cell_content(cell, allow_formatting)
+      if cell.inner_document
+        ["\n", cell.content, "\n"]
+      elsif allow_formatting
+        ['<p>', cell_text(cell), '</p>']
+      else
+        [cell.text]
+      end
+    end
+
+    def cell_text(cell)
+      return cell.text unless (style = cell.attr 'style')
+
+      cell.document.converter.convert cell, "cell_text_#{style}"
+    rescue NoMethodError
+      warn block: cell, message: "Unknown style for cell [#{style}]."
+      convert_cell_text_none cell
+    end
+
+    def convert_cell_text_emphasis(cell)
+      delegate_cell_text cell, :emphasis
+    end
+
+    def convert_cell_text_header(cell)
+      convert_cell_text_strong cell
+    end
+
+    def convert_cell_text_literal(cell)
+      delegate_cell_text cell, :literal
+    end
+
+    def convert_cell_text_monospaced(cell)
+      delegate_cell_text cell, :monospaced
+    end
+
+    def convert_cell_text_none(cell)
+      cell.text
+    end
+
+    def convert_cell_text_strong(cell)
+      delegate_cell_text cell, :strong
+    end
+
+    def convert_cell_text_verse(cell)
+      delegate_cell_text cell, :verse
+    end
+
+    private
+
+    def delegate_cell_text(cell, type)
+      Asciidoctor::Inline.new(
+        cell.parent, :quoted, cell.text, type: type
+      ).convert
     end
   end
 end
