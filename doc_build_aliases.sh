921ceaee407db5955797d9c1bdd9730a9d6407cc
Nik Everett <nik9000@gmail.com>
2019-12-17 14:26:27 -0500

    Switch go apm agent to direct_html (#1642)
    direct_html is faster and more customizable. It is the future!

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -123,7 +123,7 @@ alias docbldamj='$GIT_HOME/docs/build_docs --direct_html --doc $GIT_HOME/apm-age
 
 alias docbldamjs='$GIT_HOME/docs/build_docs --direct_html --doc $GIT_HOME/apm-agent-rum-js/docs/index.asciidoc --chunk 1'
 
-alias docbldamgo='$GIT_HOME/docs/build_docs --doc $GIT_HOME/apm-agent-go/docs/index.asciidoc --chunk 1'
+alias docbldamgo='$GIT_HOME/docs/build_docs --direct_html --doc $GIT_HOME/apm-agent-go/docs/index.asciidoc --chunk 1'
 
 alias docbldamnet='$GIT_HOME/docs/build_docs --direct_html --doc $GIT_HOME/apm-agent-dotnet/docs/index.asciidoc --chunk 1'
 
