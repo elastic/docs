b4d0bb8d524765d34420e5edee970c376d076a9b
Nik Everett <nik9000@gmail.com>
2019-12-19 11:11:38 -0500

    Share a test utility
    This moves the `file_exist` matcher into a place so the Asciidoctor
    plugin unit tests and the integration tests can share it.

--- resources/asciidoctor/spec/spec_helper.rb
+++ resources/asciidoctor/spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../../test/matcher/file_exist'
 require 'docbook45/converter'
 
 RSpec.configure do |config|
@@ -136,22 +137,5 @@ RSpec.shared_context 'Dsl_file' do |file_name|
 
   it 'is created' do
     expect(file).to file_exist
-  end
-end
-##
-# Match paths that refer to an existing file.
-# Prefer this instead of `expect(File).to exist('path')` because the failure
-# message is worlds better
-RSpec::Matchers.define :file_exist do
-  match do |actual|
-    File.exist? actual
-  end
-  failure_message do |actual|
-    msg = "expected that #{actual} exists"
-    parent = File.expand_path '..', actual
-    parent = File.expand_path '..', parent until Dir.exist? parent
-
-    entries = Dir.entries(parent).reject { |e| e.start_with? '.' }
-    msg + " but only #{parent}/#{entries.sort} exist"
   end
 end
