79ed0805bdbb44821e5a00dfb5312069b7a07a1c
Nik Everett <nik9000@gmail.com>
2019-12-20 10:33:41 -0500

    Fix test bug
    The single doc build is now so fast that it uncovered a timing issue in
    the test for `--open` where we handn't waited for parcel to start.

--- integtest/spec/helper/opened_docs.rb
+++ integtest/spec/helper/opened_docs.rb
@@ -13,6 +13,9 @@ class OpenedDocs < ServingDocs
     super cmd
 
     wait_for_logs(/start worker processes$/, 60)
-    wait_for_logs(/^preview server is listening on 3000$/, 60) if uses_preview
+    return unless uses_preview
+
+    wait_for_logs(/^preview server is listening on 3000$/, 60)
+    wait_for_logs(/  Built in /, 60) # Wait for parcel as well
   end
 end
