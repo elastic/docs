83c6e304043a67fccb914b0a54e666a50a73179d
Brandon Morelli <brandon.morelli@elastic.co>
2020-07-13 15:01:08 -0700

    test revert

--- resources/asciidoctor/lib/docbook_compat/convert_admonition.rb
+++ resources/asciidoctor/lib/docbook_compat/convert_admonition.rb
@@ -56,11 +56,37 @@ module DocbookCompat
     end
 
     def convert_inline_admonition_for_real(node)
+
+      # # If creating a defintiion admonition, maintain normal style
+      # if node.type == 'definition'
+      #   title_classes =
+      #     "Admonishment-#{node.attr 'title_type'} #{node.attr 'title_class'}"
+      #   [
+      #     %(<span class="Admonishment Admonishment--#{node.type}">),
+      #     %(<span class="#{title_classes}">#{node.attr 'title'}</span>),
+      #     '<span class="Admonishment-detail">',
+      #     node.text,
+      #     '</span>',
+      #     '</span>',
+      #   ].join "\n"
+      # # For all other admonitions, add custom admonishment style
+      # else
+      #   title_classes =
+      #     "Admonishment-#{node.attr 'title_type'} #{node.attr 'title_class'}"
+      #   [
+      #     %(<span class="Admonishment Admonishment--#{node.type}">),
+      #     %([<span class="#{title_classes}">#{node.attr 'title'}</span>]),
+      #     '<span class="Admonishment-detail">',
+      #     node.text,
+      #     '</span>',
+      #     '</span>',
+      #   ].join "\n"
+      # end
       title_classes =
         "Admonishment-#{node.attr 'title_type'} #{node.attr 'title_class'}"
       [
         %(<span class="Admonishment Admonishment--#{node.type}">),
-        %(<span class="#{title_classes}">#{node.attr 'title'}</span>),
+        %([<span class="#{title_classes}">#{node.attr 'title'}</span>]),
         '<span class="Admonishment-detail">',
         node.text,
         '</span>',
