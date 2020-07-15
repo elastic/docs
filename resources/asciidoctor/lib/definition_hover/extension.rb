0adbe37c24ed9c6d7cf937132eb94aaed3176e52
Brandon Morelli <brandon.morelli@elastic.co>
2020-07-15 11:55:27 -0700

    wow code much clean

--- resources/asciidoctor/lib/definition_hover/extension.rb
+++ resources/asciidoctor/lib/definition_hover/extension.rb
@@ -12,19 +12,12 @@ require 'asciidoctor/extensions'
 #   I like to definition:[run,To move at a speed faster than a walk.]
 #
 class DefinitionAdmonition < Asciidoctor::Extensions::Group
-  MACRO_CONF = [
-    [:definition, 'word', 'definition'],
-  ].freeze
   def activate(registry)
-    MACRO_CONF.each do |(name, word, definition)|
-      inline = ChangeAdmonitionInline.new word, definition
-      registry.inline_macro inline, name
-    end
+    registry.inline_macro Definition.new :definition
   end
 
-  ##
   # Inline change admonition.
-  class ChangeAdmonitionInline < Asciidoctor::Extensions::InlineMacroProcessor
+  class Definition < Asciidoctor::Extensions::InlineMacroProcessor
     use_dsl
 
     # Stores content passed in from asciidoc in `attrs[:x]`
@@ -32,14 +25,6 @@ class DefinitionAdmonition < Asciidoctor::Extensions::Group
 
     # I don't know what this does, but it's necessary for this widget to work
     format :short
-
-    def initialize(word, definition)
-      super(nil)
-      # I have no idea if this is necessary
-      # I also have no idea what it does
-      @word = word
-      @definition = definition
-    end
 
     def process(parent, _target, attrs)
       # Access attributes passed in from raw asciidoc
@@ -50,10 +35,6 @@ class DefinitionAdmonition < Asciidoctor::Extensions::Group
       Asciidoctor::Inline.new(
         parent, :admonition, message, type: 'definition', attributes: {
           'input_word' => input_word,
-
-          # These aren't needed. I'll delete them soon enough
-          # 'word' => @word,
-          # 'definition' => @definition,
         }
       )
     end
