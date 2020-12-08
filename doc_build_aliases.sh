46d3960db9dd25733338d76c0306f33704138710
Brandon Morelli <brandon.morelli@elastic.co>
2020-12-08 13:08:12 -0800

    docs: add ecs-logging

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -194,7 +194,7 @@ alias docbldecs='$GIT_HOME/docs/build_docs --doc $GIT_HOME/ecs/docs/index.asciid
 
 # ECS logging
 
-alias docbldecsjv='$GIT_HOME/docs/build_docs --doc $GIT_HOME/ecs-logging-java/docs/index.asciidoc --chunk 1'
+alias docbldecsjv='$GIT_HOME/docs/build_docs --doc $GIT_HOME/ecs-logging-java/docs/index.asciidoc --resource=$GIT_HOME/ecs-logging/docs/ --chunk 1'
 
 # GKE
 alias docbldgke='$GIT_HOME/docs/build_docs --doc $GIT_HOME/stack-docs/docs/en/gke-on-prem/index.asciidoc --chunk 1'
