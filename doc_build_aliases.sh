6bf858a68306b8e442c8019481f76c91e783ce2e
dedemorton <dede.morton@elastic.co>
2021-03-31 11:50:56 -0700

    Change chunk level for Fleet User Guide

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -139,7 +139,7 @@ alias docbldfnb='$GIT_HOME/docs/build_docs --respect_edit_url_overrides --doc $G
 alias docbldjb='$GIT_HOME/docs/build_docs --respect_edit_url_overrides --doc $GIT_HOME/beats/journalbeat/docs/index.asciidoc --chunk 1'
 
 # Fleet user guide
-alias docbldim='$GIT_HOME/docs/build_docs --doc $GIT_HOME/observability-docs/docs/en/ingest-management/index.asciidoc --resource=$GIT_HOME/beats/x-pack/elastic-agent/docs --resource=$GIT_HOME/apm-server/docs --chunk 1'
+alias docbldim='$GIT_HOME/docs/build_docs --doc $GIT_HOME/observability-docs/docs/en/ingest-management/index.asciidoc --resource=$GIT_HOME/beats/x-pack/elastic-agent/docs --resource=$GIT_HOME/apm-server/docs --chunk 2'
 
 # APM
 alias docbldamg='$GIT_HOME/docs/build_docs --doc $GIT_HOME/apm-server/docs/guide/index.asciidoc --chunk 1'
