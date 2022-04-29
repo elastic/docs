be90ebe490a1b419584595330429ebcfff500a30
David Kilfoyle <41695641+kilfoyle@users.noreply.github.com>
2022-04-29 13:50:57 -0400

    Update doc_build_aliases.sh
    Co-authored-by: Dan Roscigno <dan@roscigno.com>

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -120,7 +120,7 @@ alias docbldece='$GIT_HOME/docs/build_docs --doc $GIT_HOME/cloud/docs/cloud-ente
 alias docbldech='$GIT_HOME/docs/build_docs --doc $GIT_HOME/cloud/docs/heroku/index.asciidoc --resource=$GIT_HOME/cloud/docs/shared --resource=$GIT_HOME/cloud/docs/saas --chunk 1'
 
 # Cloud - Elastic Cloud on Kubernetes
-alias docbldeck='$GIT_HOME/docs/build_docs --doc $GIT_HOME/cloud-on-k8s/docs/index.asciidoc --chunk 1 --open
+alias docbldeck='$GIT_HOME/docs/build_docs --doc $GIT_HOME/cloud-on-k8s/docs/index.asciidoc --chunk 1'
 
 # Cloud - Elastic Cloud Control
 alias docbldecctl='$GIT_HOME/docs/build_docs --doc $GIT_HOME/ecctl/docs/index.asciidoc --chunk 1'
