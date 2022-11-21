7cc37747cc7806802f8c3a47d02a1574b1913a92
lcawl <lcawley@elastic.co>
2022-11-21 13:04:06 -0800

    Fix linting errors

--- resources/asciidoctor/lib/glob_include_processor/extension.rb
+++ resources/asciidoctor/lib/glob_include_processor/extension.rb
@@ -4,16 +4,16 @@ require 'asciidoctor/extensions'
 ##
 # Include multiple files in a directory using globbing
 class GlobIncludeProcessor < Asciidoctor::Extensions::IncludeProcessor
-    def process doc, reader, target_glob, attributes
-      Dir[File.join reader.dir, target_glob].sort.reverse_each do |target|
-        content = IO.readlines target
-        content.unshift '' unless attributes['adjoin-option']
-        reader.push_include content, target, target, 1, attributes
-      end
-      reader
+  def process(_doc, reader, target_glob, attributes)
+    Dir[File.join reader.dir, target_glob].sort.reverse_each do |target|
+      content = IO.readlines target
+      content.unshift '' unless attributes['adjoin-option']
+      reader.push_include content, target, target, 1, attributes
     end
-  
-    def handles? target
-      target.include? '*'
-    end
+    reader
   end
+
+  def handles?(target)
+    target.include? '*'
+  end
+end
