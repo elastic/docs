9b2ae5f93305219d3893a04acdb705f7b2987932
bmorelli25 <brandon.morelli@elastic.co>
2022-12-13 17:19:34 -0800

    it actually works

--- resources/asciidoctor/lib/docbook_compat/extra_docinfo.rb
+++ resources/asciidoctor/lib/docbook_compat/extra_docinfo.rb
@@ -16,20 +16,28 @@ module DocbookCompat
     private
 
     def extra_docbook_compat_head
+
+      # Allows :meta-product-name: to overwrite the product name
+      product_name = attributes['meta-product-name'] ? (attributes['meta-product-name']) : (attributes['dc.subject'])
+      # Uses docdir attr to match final portion of string after `en/`
+      website_area = attributes['docdir'].scan(/(?<=en\/).*/i)[0].to_s
+      # Assigns the arbitrary value of 100 if the branch being built is "current"
+      current_version_val = attributes['source_branch'] == attributes['current'] ? (100) : (0)
+      # Keeping this for now but will remove later
+      # current_version_val = attributes['is-current-version'] ? (100) : (0)
+
       [
-        # Elastic
-        elastic_compat_meta('product_name', attributes['dc.subject']),
+        # Working
+        elastic_compat_meta('website_area', website_area),
         elastic_compat_meta('product_version', attributes['dc.identifier']),
-        elastic_compat_meta('website_area', attributes['dc.prefix']),
-        elastic_compat_meta('source_branch', attributes['source_branch']),
-        elastic_compat_meta('current', attributes['current']),
+        elastic_compat_meta('product_name', product_name),
+        elastic_compat_meta('is_current_product_version', current_version_val),
 
-        if attributes['source_branch'] == attributes['current']
-          elastic_compat_meta('is_current_product_version', 100)
-        else
-          elastic_compat_meta('is_current_product_version', 0)
-        end,
+        # Not working
+        elastic_compat_meta('content', "this is blank for now"),
+        elastic_compat_meta('thumbnail_image', "this is blank for now"),
 
+        # Legacy docbook meta
         docbook_compat_meta('DC.type', attributes['dc.type']),
         docbook_compat_meta('DC.subject', attributes['dc.subject']),
         docbook_compat_meta('DC.identifier', attributes['dc.identifier']),
