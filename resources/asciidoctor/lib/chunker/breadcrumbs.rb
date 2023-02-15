70239063abbd9be60d1e322f0507c7232dca8826
Colleen McGinnis <colleen.mcginnis@elastic.co>
2023-02-15 16:23:59 -0600

    clean up, rework ecs breadcrumbs

--- resources/asciidoctor/lib/chunker/breadcrumbs.rb
+++ resources/asciidoctor/lib/chunker/breadcrumbs.rb
@@ -13,49 +13,48 @@ module Chunker
         <span class="chevron-right">›</span>
       HTML
       result = ['<div class="breadcrumbs">']
-      result += generate_breadcrumb_links(section).reverse
+      result += generate_breadcrumb_links(section, chev).reverse
       result << '</div>'
       if result[2].to_s.include? 'APM'
-        result[2] = chev + generate_apm_breadcrumbs(doc, section)
+        result[2] = chev + generate_apm_breadcrumbs(doc)
       end
       if result[2].to_s.include? 'ECS Logging'
-        result.insert(2, generate_ecslogging_breadcrumbs)
+        result[2] = chev + generate_ecslogging_breadcrumbs(doc)
       end
       Asciidoctor::Block.new doc, :pass, source: result.join("\n")
     end
 
-    def generate_apm_breadcrumbs(doc, section)
-      parent = section
+    def generate_apm_breadcrumbs(doc)
       title = doc.title
       short = title.sub(/APM /, '')
       <<~HTML.strip
         <span class="breadcrumb-link">
           <div id="related-products" class="dropdown">
             <div class="related-products-title">APM:</div>
-            <div class="dropdown-anchor">#{short}<span class="dropdown-icon"></span></div>
+            <div class="dropdown-anchor" tabindex="0">#{short}<span class="dropdown-icon"></span></div>
             <div class="dropdown-content">
               <ul>
                 <li class="dropdown-category">APM</li>
                 <ul>
-                  <li><a id="guide" href="https://www.elastic.co/guide/en/apm/guide/current/index.html">User Guide</a></li>
+                  <li><a href="/guide/en/apm/guide/current/index.html">User Guide</a></li>
                 </ul>
                 <li class="dropdown-category">APM agents</li>
                 <ul>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/android/current/index.html">Android Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/go/current/index.html">Go Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/swift/current/index.html">iOS Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/java/current/index.html">Java Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/dotnet/current/index.html">.NET Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/nodejs/current/index.html">Node.js Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/php/current/index.html">PHP Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/python/current/index.html">Python Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/ruby/current/index.html">Ruby Agent Reference</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/agent/rum-js/current/index.html">Real User Monitoring JavaScript Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/android/current/index.html">Android Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/go/current/index.html">Go Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/swift/current/index.html">iOS Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/java/current/index.html">Java Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/dotnet/current/index.html">.NET Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/nodejs/current/index.html">Node.js Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/php/current/index.html">PHP Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/python/current/index.html">Python Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/ruby/current/index.html">Ruby Agent Reference</a></li>
+                  <li><a href="/guide/en/apm/agent/rum-js/current/index.html">Real User Monitoring JavaScript Agent Reference</a></li>
                 </ul>
                 <li class="dropdown-category">APM extensions</li>
                 <ul>
-                  <li><a href="https://www.elastic.co/guide/en/apm/lambda/current/index.html">Monitoring AWS Lambda Functions</a></li>
-                  <li><a href="https://www.elastic.co/guide/en/apm/attacher/current/index.html">Attacher</a></li>
+                  <li><a href="/guide/en/apm/lambda/current/index.html">Monitoring AWS Lambda Functions</a></li>
+                  <li><a href="/guide/en/apm/attacher/current/index.html">Attacher</a></li>
                 </ul>
               </ul>
           </div>
@@ -63,37 +62,37 @@ module Chunker
       HTML
     end
 
-    def generate_ecslogging_breadcrumbs
+    def generate_ecslogging_breadcrumbs(doc)
+      title = doc.title
+      short = title.sub(/ECS Logging /, '')
       <<~HTML.strip
-        <span class="breadcrumb-link">
-          <div class="dropdown">
-            <span>ECS Logging ▾</span>
-              <div class="dropdown-content">
-                <strong>ECS Logging</strong></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/overview/current/index.html">Overview</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/go-logrus/current/index.html">Go (Logrus)</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/go-zap/current/index.html">Go (Zap)</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/java/current/index.html">Java</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/dotnet/current/index.html">.NET</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/nodejs/current/index.html">Node.js</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/ruby/current/index.html">Ruby</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/php/current/index.html">PHP</a></br>
-                <a href="https://www.elastic.co/guide/en/ecs-logging/python/current/index.html">Python</a></br>
-            </div>
-          </div>
+      <span class="breadcrumb-link">
+      <div id="related-products" class="dropdown">
+        <div class="related-products-title">ECS Logging:</div>
+        <div class="dropdown-anchor" tabindex="0">#{short}<span class="dropdown-icon"></span></div>
+        <div class="dropdown-content">
+          <ul>
+            <li><a href="/guide/en/ecs-logging/overview/current/index.html">Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/go-logrus/current/index.html">Go (Logrus) Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/go-zap/current/index.html">Go (zap) Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/java/current/index.html">Java Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/dotnet/current/index.html">.NET Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/nodejs/current/index.html">Node.js Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/ruby/current/index.html">Ruby Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/php/current/index.html">PHP Reference</a></li>
+            <li><a href="/guide/en/ecs-logging/python/current/index.html">Python Reference</a></li>
+        </div>
+      </div>
       HTML
     end
 
-    def generate_breadcrumb_links(section)
+    def generate_breadcrumb_links(section, chev)
       result = []
       parent = section
       while (parent = parent.parent)
         extra = parent.context == :document ? parent.attr('title-extra') : ''
         link = <<~HTML.strip
           <span class="breadcrumb-link"><a #{link_href parent}>#{parent.title}#{extra}</a></span>
-        HTML
-        chev = <<~HTML.strip
-          <span class="chevron-right">›</span>
         HTML
         links = chev + link
         result << links
