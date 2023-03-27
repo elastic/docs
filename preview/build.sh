d5127acfcbdb614b8a5c3cbf6e9f0bddb7d1accd
Greg Back <gregory.back@elastic.co>
2023-03-27 13:10:15 -0400

    Update Preview image
    We need a new image with the latest GitHub RSA SSH public key in order
    to fetch docs with Git in order to preview them.
    
    Update the scripts a bit in the meantime to reflect what I did when
    building/testing/pushing this.

--- preview/build.sh
+++ preview/build.sh
@@ -4,9 +4,14 @@
 
 set -eo pipefail
 
-export PREVIEW=docker.elastic.co/docs/preview:15
+export IMAGE=docker.elastic.co/docs/preview
+export VERSION=17
 
 cd $(git rev-parse --show-toplevel)
 ./build_docs --docker-build build
-DOCKER_BUILDKIT=1 docker build -t $PREVIEW -f preview/Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t $IMAGE:$VERSION -f preview/Dockerfile .
 
+docker tag $IMAGE:$VERSION $IMAGE:latest
+
+# docker push $IMAGE:$VERSION
+# docker push $IMAGE:latest
