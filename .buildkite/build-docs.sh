194bd25896687d6e6b4979021dcf2955a3a1ff2f
nassimkammah <nassim.kammah@elastic.co>
2023-09-29 13:30:54 +0200

    Add more parameters checking

--- .buildkite/build-docs.sh
+++ .buildkite/build-docs.sh
@@ -10,6 +10,10 @@ export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
 export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 
 
+build_args=""
+rebuild_opt=""
+skiplinkcheck_opt=""
+warnlinkcheck_opt=""
 # From https://github.com/elastic/infra/blob/master/ci/jjb/elasticsearch-ci/defs/elastic-docs/pull-requests.yml#L110
 # Per https://github.com/elastic/docs/issues/1821, always rebuild all
 # books for PRs to the docs repo, for now.
@@ -17,42 +21,40 @@ export GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL
 # should be removed and the original behavior restored.
 #rebuild_opt="--rebuild"
 
-# rebuild_opt=""
-# if [[ "${ghprbCommentBody}" == *rebuild* ]]; then
-#   rebuild_opt="--rebuild"
-# fi
+if [[ "${REBUILD}" == 'true' ]]; then
+  rebuild_opt="--rebuild"
+fi
 
-#skiplinkcheck_opt=""
-#if [[ "${ghprbCommentBody}" == *skiplinkcheck* ]]; then
-#  skiplinkcheck_opt="--skiplinkcheck"
-#fi
-#
-#warnlinkcheck_opt=""
-#if [[ "${ghprbCommentBody}" == *warnlinkcheck* ]]; then
-#  warnlinkcheck_opt="--warnlinkcheck"
-#fi
-docker images
+if [[ "${SKIP_LINK_CHECK}" == 'true' ]]; then
+  skiplinkcheck_opt="--skiplinkcheck"
+fi
 
-git clone \
-  --reference /opt/git-mirrors/elastic-docs \
-  git@github.com:elastic/docs.git .docs
+if [[ "${ALLOW_BROKEN_LINKS}" == 'true' ]]; then
+  warnlinkcheck_opt="--warnlinkcheck"
+fi
 
-ls -lart
+#git clone \
+#  --reference /opt/git-mirrors/elastic-docs \
+#  git@github.com:elastic/docs.git .docs
 
 
-ls -lart /opt/git-mirrors/
+# When running on a branch or on main
+if [[ "${GIT_PULL_REQUEST_ID}" != "false" ]]; then
+  # temporary pushing to a branch different than main until ready to switchover
+  build_args+= " --target_branch docs_bk_rollout"
+else
+  build_args+= " --target_branch docs_bk_${GIT_PULL_REQUEST_ID}"
+  build_args+= " --announce_preview https://docs_bk_${GIT_PULL_REQUEST_ID}.docs-preview.app.elstc.co/diff"
+  rebuild_opt= " --rebuild"
+fi
+
 # The docs build can use the ssh agent's authentication socket
 # but can't use ssh keys directly so we start an ssh-agent.
-
-ghprbPullId=123
-
 ssh-agent bash -c "
   ssh-add &&
   ./build_docs --all \
     --target_repo git@github.com:elastic/built-docs \
-    --target_branch docs_bk_${ghprbPullId} \
-    --announce_preview https://docs_bk_${ghprbPullId}.docs-preview.app.elstc.co/diff \
-    --announce_preview https://docs_bk_${ghprbPullId}.docs-preview.app.elstc.co/diff \
-    --push \
+    $build_args \
+    $rebuild_opt $skiplinkcheck_opt $warnlinkcheck_opt \
     --reference /opt/git-mirrors/"
-#    $rebuild_opt $skiplinkcheck_opt $warnlinkcheck_opt"
+# --push
