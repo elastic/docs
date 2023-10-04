7911c967415d47bddfc98ed4487f11e520a51dce
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-10-04 14:53:27 +0300

    removed symlink POC code

--- .buildkite/scripts/build-docs.sh
+++ .buildkite/scripts/build-docs.sh
@@ -38,22 +38,8 @@ else
   build_args+=" --announce_preview https://docs_bk_${BUILDKITE_PULL_REQUEST}.docs-preview.app.elstc.co/diff"
 fi
 
-
-## Temporary hack to test the symlink on reference repos
-cd /opt/git-mirrors/
-#for repo in elastic-*;do
-#    echo $repo
-#    ln -s $repo ${repo:8}.git
-#done
-#cd -
-ls -lart /opt/git-mirrors/
-
 # The docs build can use the ssh agent's authentication socket
 # but can't use ssh keys directly so we start an ssh-agent.
-
-
-exit 0
-
 ssh-agent bash -c "
   ssh-add &&
   ./build_docs --all \
