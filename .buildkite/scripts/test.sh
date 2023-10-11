b27501f52932eaf3c4142f7d14a3ff95912d1aac
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-10-11 16:05:24 +0300

    fixed PR based on comments

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-#cd $(git rev-parse --show-toplevel)
-
 echo "Building images for docs test"
 for IMAGE in build py_test node_test ruby_test integ_test diff_tool; do
   echo Building $IMAGE
