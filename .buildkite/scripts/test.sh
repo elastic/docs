c2f883f1247bf561fdd18102b5af3e1124dfadc2
Olga Naydyonock <olga.naidjonoka@elastic.co>
2023-10-11 18:00:52 +0300

    Test job migration (#2790)
    * added test execution script
    
    * temporarly commented execution only on master
    
    * changed pr_main_mr_test.yml
    
    * fixed script path
    
    * changed script command definition
    
    * added agent to yml
    
    * enabled execution on main only
    
    * added slack notifications
    
    * commented cd from script
    
    * temporarly commented master only build
    
    * fixed PR based on comments

---
+++ .buildkite/scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Building images for docs test"
+for IMAGE in build py_test node_test ruby_test integ_test diff_tool; do
+  echo Building $IMAGE
+  ./build_docs --docker-build $IMAGE
+done
+
+make
