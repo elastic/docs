b3e508ca6e6a7200b86e5304e2ca4d1293a52c4a
Aliaksandr Trushyn <aliaksandr.trushyn@elastic.co>
2023-10-11 12:01:46 +0200

    Migrate build air-gapped job

---
+++ air_gapped/build_bk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Build the docker image for the air gapped docs.
+
+set -eo pipefail
+
+cd $(git rev-parse --show-toplevel)
+
+if [[ ! -d /opt/git-mirrors/built-docs.git ]]; then
+  echo "/opt/git-mirrors/built-docs.git must exist and contain a reference clone of the built-docs repo"
+  exit 1
+fi
+
+# Get an up to date copy of the repo
+mkdir air_gapped/work
+git clone --reference /opt/git-mirrors/built-docs.git --dissociate \
+  --depth 2 --branch master --bare \
+  git@github.com:elastic/built-docs.git air_gapped/work/target_repo.git
+GIT_DIR=air_gapped/work/target_repo.git git fetch
+
+# Build the images
+source preview/build.sh
+DOCKER_BUILDKIT=1 docker build -t $AIR_GAPPED -f air_gapped/Dockerfile .
