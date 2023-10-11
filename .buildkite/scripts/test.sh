cac9fadf0dd02c4acb0604f66160c0f009e8c249
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-10-11 15:39:44 +0300

    commented cd from script

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-cd $(git rev-parse --show-toplevel)
+#cd $(git rev-parse --show-toplevel)
 
 echo "Building images for docs test"
 for IMAGE in build py_test node_test ruby_test integ_test diff_tool; do
