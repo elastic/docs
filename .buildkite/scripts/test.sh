11f78897a5c706bb2893c8a2734006dc44832c9e
Olga Naidjonoka <olga_naidjonoka@epam.com>
2023-10-11 11:26:04 +0300

    changed pr_main_mr_test.yml

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 cd $(git rev-parse --show-toplevel)
 
-echo "Building images for test execution"
+echo "Building images for docs test"
 for IMAGE in build py_test node_test ruby_test integ_test diff_tool; do
   echo Building $IMAGE
   ./build_docs --docker-build $IMAGE
