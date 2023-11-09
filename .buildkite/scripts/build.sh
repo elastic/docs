b2e1f998608b50aef6011a7e012eabda8c5210cb
nassimkammah <nassim.kammah@elastic.co>
2023-11-09 16:21:01 +0100

    update build script

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -14,31 +14,21 @@ build_args=""
 rebuild_opt=""
 broken_links_opt=""
 
-# From https://github.com/elastic/infra/blob/master/ci/jjb/elasticsearch-ci/defs/elastic-docs/pull-requests.yml#L110
-# Per https://github.com/elastic/docs/issues/1821, always rebuild all
-# books for PRs to the docs repo, for now.
-# When https://github.com/elastic/docs/issues/1823 is fixed, this
-# should be removed and the original behavior restored.
-
-if [[ "${REBUILD}" == 'true' ]]; then
+if [[ "${REBUILD}" == 'rebuild' ]]; then
   rebuild_opt="--rebuild"
 fi
 
-if [[ "${BROKEN_LINKS}" == 'skip' ]]; then
+if [[ "${BROKEN_LINKS}" == 'skiplinkcheck' ]]; then
   broken_links_opt="--skiplinkcheck"
-fi
-
-if [[ "${BROKEN_LINKS}" == 'warn' ]]; then
-  broken_links_opt="--warnlinkcheck"
-fi
-
-# When running on a branch or on main
-if [[ "${BUILDKITE_PULL_REQUEST}" == "false" ]]; then
-  # temporary pushing to staging instead of master until the migration is over
-  build_args+=" --target_branch staging"
 else
-  build_args+=" --target_branch docs_bk_${BUILDKITE_PULL_REQUEST}"
-  build_args+=" --announce_preview https://docs_bk_${BUILDKITE_PULL_REQUEST}.docs-preview.app.elstc.co/diff"
+  if [[ "${BROKEN_LINKS}" == 'warnlinkcheck' ]]; then
+    broken_links_opt="--warnlinkcheck"
+  fi
+fi
+
+if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
+  # temporary pushing to staging instead of master until the migration is over
+  build_args+=" --target_branch staging --push"
 fi
 
 # The docs build can use the ssh agent's authentication socket
@@ -48,7 +38,6 @@ ssh-agent bash -c "
   ssh-add &&
   ./build_docs --all \
     --target_repo git@github.com:elastic/built-docs \
-    $build_args \
-    $rebuild_opt $broken_links_opt \
     --reference /opt/git-mirrors/ \
-    --push"
+    $build_args \
+    $rebuild_opt $broken_links_opt"
