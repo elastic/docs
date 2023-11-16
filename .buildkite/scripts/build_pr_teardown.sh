da0d7229e6ee6434765e9263a2b79aa6e3e47113
nassimkammah <nassim.kammah@elastic.co>
2023-11-16 10:53:06 +0100

    Switch buildkite hooks to pipeline steps
    This is a more explicit approach that can be applied to the PR pipeline
    only and allows to run only once before/after the build, as opposed to
    the pre/post command hooks that run for every command.

--- .buildkite/scripts/build_pr_teardown.sh
+++ .buildkite/scripts/build_pr_teardown.sh
@@ -9,14 +9,14 @@ fi
 
 gitHubToken=$(vault read -field=value secret/ci/elastic-docs/docs_preview_cleaner)
 
-if [ -n "$BUILDKITE_COMMAND_EXIT_STATUS" ] && [ "$BUILDKITE_COMMAND_EXIT_STATUS" -eq "0" ];then
+if [ $(buildkite-agent step get "outcome" --step "build-pr") == "passed" ]; then
   status_state="success"
 else
   status_state="failure"
 fi
 
 githubPublishStatus="https://api.github.com/repos/${GITHUB_PR_OWNER}/${GITHUB_PR_REPO}/statuses/${GITHUB_PR_TRIGGERED_SHA}"
-data='{"state":"'$status_state'","target_url":"'$BUILDKITE_BUILD_URL'","description":"Build finished.","context":"buildkite/docs"}'
+data='{"state":"'$status_state'","target_url":"'$BUILDKITE_BUILD_URL'","description":"Build finished.","context":"buildkite/'$BUILDKITE_PIPELINE_SLUG'"}'
 echo "Setting buildkite/docs commit status to ${status_state}"
 curl -s -L \
   -X POST \
