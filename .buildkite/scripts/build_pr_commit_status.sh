742a6d4ad31d42637cedba646a8cc0061a795fbe
nassimkammah <nassim.kammah@elastic.co>
2023-11-17 07:17:23 +0100

    Better secrets handling
    Secrets should be defined in pre-command hooks to benefit from redaction
    See https://buildkite.com/docs/pipelines/managing-log-output#redacted-environment-variables

--- .buildkite/scripts/build_pr_commit_status.sh
+++ .buildkite/scripts/build_pr_commit_status.sh
@@ -20,8 +20,6 @@ case $status_state in
       exit 1;;
 esac
 
-gitHubToken=$(vault read -field=value secret/ci/elastic-docs/docs_preview_cleaner)
-
 githubPublishStatus="https://api.github.com/repos/${GITHUB_PR_OWNER}/${GITHUB_PR_REPO}/statuses/${GITHUB_PR_TRIGGERED_SHA}"
 data='{"state":"'$status_state'","target_url":"'$BUILDKITE_BUILD_URL'","description":"'$description'","context":"buildkite/'$BUILDKITE_PIPELINE_SLUG'"}'
 
@@ -29,7 +27,7 @@ echo "Setting buildkite/docs commit status to ${status_state}"
 curl -s -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${gitHubToken}" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "${githubPublishStatus}" \
   -d "${data}"
