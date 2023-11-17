068b19b78c9f85fa2531b8ae02946af5b1c86d08
nassimkammah <nassim.kammah@elastic.co>
2023-11-17 07:15:13 +0100

    Combine setup/teardown into one script

--- .buildkite/scripts/build_pr_commit_status.sh
+++ .buildkite/scripts/build_pr_commit_status.sh
@@ -2,16 +2,30 @@
 
 set -euo pipefail
 
-# The remainder of this hook should only be invoked for builds triggered by the Buildkite PR bot
+# This hook should only be invoked for builds triggered by the Buildkite PR bot
 if [ -z ${GITHUB_PR_OWNER+set} ] || [ -z ${GITHUB_PR_REPO+set} ] || [ -z ${GITHUB_PR_TRIGGERED_SHA+set} ];then
   exit 0
 fi
 
+status_state=$1
+description=''
+
+case $status_state in
+    pending)
+      description='Build started';;
+    success|failure|error)
+      description='Build finished';;
+    *)
+      echo "Invalid state $status_state"
+      exit 1;;
+esac
+
 gitHubToken=$(vault read -field=value secret/ci/elastic-docs/docs_preview_cleaner)
 
 githubPublishStatus="https://api.github.com/repos/${GITHUB_PR_OWNER}/${GITHUB_PR_REPO}/statuses/${GITHUB_PR_TRIGGERED_SHA}"
-data='{"state":"pending","target_url":"'$BUILDKITE_BUILD_URL'","description":"Build started.","context":"buildkite/'$BUILDKITE_PIPELINE_SLUG'"}'
-echo "Setting buildkite/docs commit status to pending"
+data='{"state":"'$status_state'","target_url":"'$BUILDKITE_BUILD_URL'","description":"'$description'","context":"buildkite/'$BUILDKITE_PIPELINE_SLUG'"}'
+
+echo "Setting buildkite/docs commit status to ${status_state}"
 curl -s -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
