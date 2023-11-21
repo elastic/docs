05c61f0be50a0ef96376fc5855e39ac5ec2b892d
nassimkammah <nassim.kammah@elastic.co>
2023-11-21 19:24:29 +0100

    Use bigger CI machine for docs repo
    We always do a full rebuild on `elastic/docs` which takes up to 2 hours
    with the standard n1-standard-4 CI machine. This PR introduces an
    optimization only for builds on elastic/docs to use n2-standard-32 to
    drop the build time to about 1 hour. For any other builds, we'll use
    n2-standard-4 and tolerate the <20 min build time for docs given the
    price difference (10x cheaper).

--- .buildkite/scripts/build_pr.sh
+++ .buildkite/scripts/build_pr.sh
@@ -65,7 +65,7 @@ else
   # books for PRs to the docs repo, for now.
   # When https://github.com/elastic/docs/issues/1823 is fixed, this
   # should be removed and the original behavior restored.
-  rebuild_opt=" --rebuild"
+  rebuild_opt=" --rebuild --procs 16"
 fi
 
 
