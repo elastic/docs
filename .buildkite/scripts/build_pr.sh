77f7c9dfc781d7fa286393ca69d3b2f8365e114e
nassimkammah <nassim.kammah@elastic.co>
2023-12-18 14:09:56 +0100

    Try out use-https flag

--- .buildkite/scripts/build_pr.sh
+++ .buildkite/scripts/build_pr.sh
@@ -86,7 +86,7 @@ echo "The following build command will be used"
 echo $build_cmd
 
 # Kick off the build
-ssh-agent bash -c "ssh-add && $build_cmd"
+echo ssh-agent bash -c "ssh-add && $build_cmd"
 
 buildkite-agent annotate \
   --style "success" \
