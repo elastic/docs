2a5815ecfc981268a7956e598275a57fcfdcabf2
Nassim Kammah <nkammah@gmail.com>
2024-01-29 13:31:10 +0100

    Revert "Bk use pat for commit statuses"

--- .buildkite/scripts/clean_preview_branches.sh
+++ .buildkite/scripts/clean_preview_branches.sh
@@ -2,6 +2,10 @@
 
 set -eo pipefail
 
+set +x
+export GITHUB_TOKEN=$(vault read -field=value secret/ci/elastic-docs/docs_preview_cleaner)
+set -x
+
 export REPO=git@github.com:elastic/built-docs.git
 export IMAGE=docker.elastic.co/docs/build:latest
 
@@ -13,7 +17,7 @@ ssh-agent bash -c '
         -v ~/.ssh/known_hosts:/root/.ssh/known_hosts:cached,ro \
         -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK:cached,ro \
         -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
-        -e GITHUB_TOKEN=$VAULT_GITHUB_TOKEN \
+        -e GITHUB_TOKEN=$GITHUB_TOKEN \
         -v /opt/git-mirrors:/opt/git-mirrors:cached,ro \
         -e CACHE_DIR=/opt/git-mirrors \
         $IMAGE node /docs_build/preview/clean.js $REPO'
