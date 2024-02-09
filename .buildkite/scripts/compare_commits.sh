84035af4972fc5cdee0745227119744707de1d5e
Brandon Morelli <brandon.morelli@elastic.co>
2024-02-09 11:00:26 -0800

    Set build type for scheduled builds based on doc repo changes (#2931)
    * check if docs repo has changed
    
    * more
    
    * updates so I can test it
    
    * build commit
    
    * lbc
    
    * gpb
    
    * more testing
    
    * a
    
    * b
    
    * mmhm
    
    * use auth token
    
    * fix quotes
    
    * c
    
    * aha!
    
    * no way this works
    
    * forgot the squigles
    
    * back up
    
    * perhaps
    
    * lsc
    
    * no squigs
    
    * jq isn’t working
    
    * quotes
    
    * try a file
    
    * ugh
    
    * d
    
    * e
    
    * move to file, echo all the things
    
    * Fixing up the script - mostly adding curl and jq flags
    
    * wire up the rebuild
    
    * fix script typo
    
    * export rebuild var
    
    * Make the script more generic
    
    * Set REBUILD value to false since empty strings are not accepted
    
    * Remove early exits used for testing purposes
    
    * Try setting concurrency group name based on branch
    
    * uncomment
    
    ---------
    
    Co-authored-by: nassimkammah <nassim.kammah@elastic.co>

---
+++ .buildkite/scripts/compare_commits.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+last_successful_build_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds?branch=${BUILDKITE_BRANCH}&state=passed"
+LAST_SUCCESSFUL_COMMIT=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $last_successful_build_url | jq -r '.[0].commit')
+
+echo "Comparing the current docs build commit ${BUILDKITE_COMMIT} to the last successful build commit ${LAST_SUCCESSFUL_COMMIT}"
+if [[ "$BUILDKITE_COMMIT" == "$LAST_SUCCESSFUL_COMMIT" ]]; then
+  echo "The docs repo has not changed since the last build."
+  buildkite-agent meta-data set "REBUILD" "false"
+else
+  echo "The docs repo has changed since the last build."
+  buildkite-agent meta-data set "REBUILD" "rebuild"
+fi
