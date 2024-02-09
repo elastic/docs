a4b8790fb40edfd397ea6cdc2e3763627bd494ab
bmorelli25 <brandon.morelli@elastic.co>
2024-02-09 14:13:21 -0800

    rookie mistake here, there, everywhere

--- .buildkite/scripts/check_queue.sh
+++ .buildkite/scripts/check_queue.sh
@@ -7,9 +7,7 @@ cancel_build_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/$
 # Don't look at the last build (it's okay if it's still running!)
 # Look three builds back instead (if this build is still running,
 # it means there's already one in the queue and we can safely cancel this one)
-THIRD_TO_LAST_BUILD_STATE=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $build_data_url | jq -r '.[2].status')
-
-echo $(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $build_data_url)
+THIRD_TO_LAST_BUILD_STATE=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $build_data_url | jq -r '.[2].state')
 
 echo "Determining if there are multiple builds waiting."
 if [[ "$THIRD_TO_LAST_BUILD_STATE" == "running" ]]; then
