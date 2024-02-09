31a5e1c303a3ffb8344c0e39eccd286bc584146a
bmorelli25 <brandon.morelli@elastic.co>
2024-02-09 14:07:53 -0800

    fix rookie mistake

--- .buildkite/scripts/check_queue.sh
+++ .buildkite/scripts/check_queue.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-last_build_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds?branch=${BUILDKITE_BRANCH}"
+build_data_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds?branch=${BUILDKITE_BRANCH}"
 cancel_build_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_JOB_ID}/cancel"
 
 # Don't look at this build (it's running now!)
 # Don't look at the last build (it's okay if it's still running!)
 # Look three builds back instead (if this build is still running,
 # it means there's already one in the queue and we can safely cancel this one)
-THIRD_TO_LAST_BUILD_STATE=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $last_successful_build_url | jq -r '.[2].status')
+THIRD_TO_LAST_BUILD_STATE=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $build_data_url | jq -r '.[2].status')
+
+echo $(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $build_data_url)
 
 echo "Determining if there are multiple builds waiting."
 if [[ "$THIRD_TO_LAST_BUILD_STATE" == "running" ]]; then
