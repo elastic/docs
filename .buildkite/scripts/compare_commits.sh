5bc5c78ec58fa79fb3ffc80a2c72124958e73c9c
nassimkammah <nassim.kammah@elastic.co>
2024-02-09 08:58:47 +0100

    Make the script more generic

--- .buildkite/scripts/compare_commits.sh
+++ .buildkite/scripts/compare_commits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-last_successful_build_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/docs-build/builds?branch=master&state=passed"
+last_successful_build_url="https://api.buildkite.com/v2/organizations/elastic/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds?branch=${BUILDKITE_BRANCH}&state=passed"
 LAST_SUCCESSFUL_COMMIT=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN}" $last_successful_build_url | jq -r '.[0].commit')
 
 echo "Comparing the current docs build commit ${BUILDKITE_COMMIT} to the last successful build commit ${LAST_SUCCESSFUL_COMMIT}"
