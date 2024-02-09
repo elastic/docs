164ef9634cf1082bc380a34b3c1aa4223ff8e82e
nassimkammah <nassim.kammah@elastic.co>
2024-02-09 09:03:40 +0100

    Set REBUILD value to false since empty strings are not accepted

--- .buildkite/scripts/compare_commits.sh
+++ .buildkite/scripts/compare_commits.sh
@@ -6,7 +6,7 @@ LAST_SUCCESSFUL_COMMIT=$(curl -s -H "Authorization: Bearer ${BUILDKITE_API_TOKEN
 echo "Comparing the current docs build commit ${BUILDKITE_COMMIT} to the last successful build commit ${LAST_SUCCESSFUL_COMMIT}"
 if [[ "$BUILDKITE_COMMIT" == "$LAST_SUCCESSFUL_COMMIT" ]]; then
   echo "The docs repo has not changed since the last build."
-  buildkite-agent meta-data set "REBUILD" ""
+  buildkite-agent meta-data set "REBUILD" "false"
 else
   echo "The docs repo has changed since the last build."
   buildkite-agent meta-data set "REBUILD" "rebuild"
