e0775e78f18ac6f744441ddefcd3c3ceda228f71
Nastasha Solomon <79124755+nastasha-solomon@users.noreply.github.com>
2024-03-21 17:10:44 -0400

    Update resources/web/docs_js/utils.js

--- resources/web/docs_js/utils.js
+++ resources/web/docs_js/utils.js
@@ -25,7 +25,7 @@ export function open_current(pathname) {
 export function console_regex() {
   // Port of
   // https://github.com/elastic/elasticsearch/blob/master/buildSrc/src/main/groovy/org/elasticsearch/gradle/doc/RestTestsFromSnippetsTask.groovy#L71-L79
-  var method = '(GET|PUT|POST|HEAD|OPTIONS|DELETE)';
+  var method = '(GET|PUT|POST|PATCH|HEAD|OPTIONS|DELETE)';
   var pathAndQuery = '([^\\n]+)';
   var badBody = 'GET|PUT|POST|PATCH|HEAD|OPTIONS|DELETE|#';
   var body = '((?:\\n(?!$badBody)[^\\n]+)+)'.replace('$badBody', badBody);
