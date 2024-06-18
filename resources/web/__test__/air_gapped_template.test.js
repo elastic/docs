0eae7e0cf1dafa46b8e11a2c51d67b29408863b7
Colleen McGinnis <colleen.mcginnis@elastic.co>
2024-06-18 16:48:14 -0500

    Update name of css and js files (#3017)
    * update name of css and js files
    
    * fix tests
    
    * Revert "Revert "Always use h1 for first heading on a page (#3009)" (#3018)"
    
    This reverts commit 84062306675b0458d5e1b4a23ddf10bf17931f5d.
    
    ---------
    
    Co-authored-by: bmorelli25 <brandon.morelli@elastic.co>

--- resources/web/__test__/air_gapped_template.test.js
+++ resources/web/__test__/air_gapped_template.test.js
@@ -44,8 +44,8 @@ describe("air_gapped_template.html", () => {
      * the built-docs repo.
      */
     const compiledWebResources = [
-      "/guide/static/styles.css",
-      "/guide/static/docs.js",
+      "/guide/static/styles-v1.css",
+      "/guide/static/docs-v1.js",
       "/guide/static/jquery.js"
     ];
     /**
