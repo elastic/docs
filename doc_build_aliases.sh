121dc659f8f336ec0ab08636571b91b1cddc4206
dedemorton <dede.morton@elastic.co>
2024-08-09 14:16:55 -0700

    Bump Obs Guide chunk level

--- doc_build_aliases.sh
+++ doc_build_aliases.sh
@@ -110,7 +110,7 @@ alias docbldeesr='$GIT_HOME/docs/build_docs --doc $GIT_HOME/enterprise-search-ru
 alias docbldewsn='$GIT_HOME/docs/build_docs --doc $GIT_HOME/enterprise-search-pubs/client-docs/workplace-search-node/index.asciidoc --single'
 
 # Observability Guide 8.8 and later
-alias docbldob='$GIT_HOME/docs/build_docs --doc $GIT_HOME/observability-docs/docs/en/observability/index.asciidoc --chunk 3 --resource $GIT_HOME/apm-server --resource=$GIT_HOME/ingest-docs/docs'
+alias docbldob='$GIT_HOME/docs/build_docs --doc $GIT_HOME/observability-docs/docs/en/observability/index.asciidoc --chunk 4 --resource $GIT_HOME/apm-server --resource=$GIT_HOME/ingest-docs/docs'
 
 # Observability Guide 8.7 and earlier
 alias docbldobold='$GIT_HOME/docs/build_docs --doc $GIT_HOME/observability-docs/docs/en/observability/index.asciidoc --chunk 2 --resource $GIT_HOME/beats/libbeat/docs --resource $GIT_HOME/apm-server/docs/guide --resource=$GIT_HOME/ingest-docs/docs'
