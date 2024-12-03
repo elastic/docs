c6cad757eda5f6f20e9dbcdefeef879a3b0b1a43
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-03 10:33:03 +0100

    Continue to support 7.13

--- build_docs.pl
+++ build_docs.pl
@@ -433,6 +433,9 @@ sub check_kibana_links {
         } || eval {
             $links_file = "packages/core/doc-links/core-doc-links-browser-internal/src/doc_links_service.ts";
             $repo->show_file( $link_check_name, $branch, $links_file );
+        } || eval {
+            $links_file = "src/core/public/doc_links/doc_links_service.ts";
+            $repo->show_file( $link_check_name, $branch, $links_file );
         };
         die "failed to find kibana links file;\n$@" unless $source;
 
