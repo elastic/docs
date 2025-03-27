3527106bd65721494d38798c24b273399cb27310
Jan Calanog <jan.calanog@elastic.co>
2025-03-27 21:44:00 +0100

    fix

--- resources/asciidoctor/lib/chunker/url_to_v3.rb
+++ resources/asciidoctor/lib/chunker/url_to_v3.rb
@@ -13,7 +13,7 @@ module Chunker
       # raise ArgumentError, "Missing required attribute 'current-url'" if current_url.nil?
       # raise ArgumentError, "Missing required attribute 'outdir'" if outdir.nil?
 
-      Hardcoded file path
+      # Hardcoded file path
       file_path = File.expand_path('v3-mapping.json', __dir__)
       # Read content from the specified file and convert it to a dictionary
       mapping = JSON.parse(File.read(file_path)) if File.exist?(file_path)
@@ -29,7 +29,6 @@ module Chunker
       end
 
       actual_url = '/guide' + path_dir + '/*/' + current_url
-
 
       if mapping.key?(actual_url)
         new_url = mapping[actual_url]
