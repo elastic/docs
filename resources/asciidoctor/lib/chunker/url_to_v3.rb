635cafa839e029822b6a911a8bc5580257148110
Jan Calanog <jan.calanog@elastic.co>
2025-03-27 21:19:23 +0100

    test

--- resources/asciidoctor/lib/chunker/url_to_v3.rb
+++ resources/asciidoctor/lib/chunker/url_to_v3.rb
@@ -13,10 +13,10 @@ module Chunker
       # raise ArgumentError, "Missing required attribute 'current-url'" if current_url.nil?
       # raise ArgumentError, "Missing required attribute 'outdir'" if outdir.nil?
 
-      # Hardcoded file path
-      # file_path = File.expand_path('v3-mapping.json', __dir__)
-      # # Read content from the specified file and convert it to a dictionary
-      # mapping = JSON.parse(File.read(file_path)) if File.exist?(file_path)
+      Hardcoded file path
+      file_path = File.expand_path('v3-mapping.json', __dir__)
+      # Read content from the specified file and convert it to a dictionary
+      mapping = JSON.parse(File.read(file_path)) if File.exist?(file_path)
 
       segments = outdir.sub('/tmp/docsbuild/target_repo/raw', '').split('/')
 
@@ -31,15 +31,15 @@ module Chunker
       actual_url = '/guide' + path_dir + '/*/' + current_url
 
 
-      # if mapping.key?(actual_url)
-      #   new_url = mapping[actual_url]
-      # else
-        new_url = '/docs'
-      # end
+      if mapping.key?(actual_url)
+        new_url = mapping[actual_url]
+      else
+       new_url = '/docs'
+      end
 
       @header = Asciidoctor::Block.new(doc, :pass, source: <<~HTML)
       <div id="url-to-v3">
-        From: <a href="#{actual_url}">CURRENT URL</a>
+        From: #{actual_url}
         To: <a href="#{new_url}">NEW URL</a>
       </div>
       HTML
