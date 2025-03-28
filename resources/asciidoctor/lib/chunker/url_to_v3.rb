03023326d8e49c639a1868c15b6f6b6b4723a64b
Jan Calanog <jan.calanog@elastic.co>
2025-03-28 17:40:29 +0100

    Refactor to satisfy rubocop

--- resources/asciidoctor/lib/chunker/url_to_v3.rb
+++ resources/asciidoctor/lib/chunker/url_to_v3.rb
@@ -1,33 +1,48 @@
+# frozen_string_literal: true
+
 module Chunker
-
+  # Add a warning to the page with a link to docs v3
   class UrlToV3
-
     attr_reader :url
 
     def initialize(doc)
       current_url = doc.attr('current-url')
       outdir = doc.attr('outdir')
       current_url ||= 'index.html'
-      file_path = File.expand_path('v3-mapping.json', __dir__)
-      mapping = JSON.parse(File.read(file_path)) if File.exist?(file_path)
-      segments = outdir.sub('/tmp/docsbuild/target_repo/raw', '').split('/') # This only works in CI. 
+      m = mapping
+      # This only works in CI.
+      segments = outdir.sub('/tmp/docsbuild/target_repo/raw', '').split('/')
       version = segments[-1] || 'unknown'
+      actual_url = get_actual_url(outdir, current_url)
+      new_url = if m.key?(actual_url)
+                  m[actual_url]
+                else
+                  '/docs'
+                end
+      render_warning(doc, version, new_url)
+    end
 
-      path_dir = if segments.empty?
+    def mapping
+      file_path = File.expand_path('v3-mapping.json', __dir__)
+      JSON.parse(File.read(file_path)) if File.exist?(file_path)
+    end
+
+    def get_path_dir(outdir)
+      segments = outdir.sub('/tmp/docsbuild/target_repo/raw', '').split('/')
+      if segments.empty?
         ''
       elsif segments.length > 1
         segments[0...-1].join('/')
       else
         segments[0]
       end
+    end
 
-      actual_url = path_dir + '/*/' + current_url
-      new_url = if mapping.key?(actual_url)
-        mapping[actual_url]
-      else
-        '/docs'
-      end
+    def get_actual_url(outdir, current_url)
+      get_path_dir(outdir) + '/*/' + current_url
+    end
 
+    def render_warning(doc, version, new_url)
       if version == '8.18'
         @url = Asciidoctor::Block.new(doc, :pass, source: <<~HTML)
           <div id="url-to-v3" style="version-warning">
