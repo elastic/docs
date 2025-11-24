968d4d5e29c689ef80dd7d14135bc396bf91a508
Jan Calanog <jan.calanog@elastic.co>
2025-11-24 10:43:41 +0100

    Fix nested css

--- resources/web/postcss.config.js
+++ resources/web/postcss.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   plugins: [
     require('postcss-import')(),
-    require('precss')(),
+    require('postcss-nested')(),
     require('postcss-assets')({
       loadPaths: [path.join(__dirname, 'style')],
     }),
