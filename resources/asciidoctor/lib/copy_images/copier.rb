04577a6bf9e62157628eb1e35016f29e612d0f85
Martijn Laarman <Mpdreamz@gmail.com>
2026-05-03 19:43:08 +0200

    Shorten warn messages to restore MethodLength to 20 lines

--- resources/asciidoctor/lib/copy_images/copier.rb
+++ resources/asciidoctor/lib/copy_images/copier.rb
@@ -51,14 +51,12 @@ module CopyImages
       destination = File.expand_path(File.join(to_dir, uri))
 
       unless destination.start_with?("#{to_dir}/")
-        warn block: block,
-             message: "Refusing to copy image outside output dir: #{uri}"
+        warn block: block, message: "Image path escapes output dir: #{uri}"
         return
       end
 
       if File.symlink?(destination)
-        warn block: block,
-             message: "Refusing to overwrite symlink at dest: #{destination}"
+        warn block: block, message: "Destination is a symlink: #{destination}"
         return
       end
 
