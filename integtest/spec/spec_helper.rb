5ce7cf1dcef3974eef768d63dee2df1ed7d716a5
Martijn Laarman <Mpdreamz@gmail.com>
2026-05-03 20:15:55 +0200

    Add tmpfs for parcel caches and fix rspec_status path

--- integtest/spec/spec_helper.rb
+++ integtest/spec/spec_helper.rb
@@ -23,7 +23,7 @@ ENV['GIT_COMMITTER_EMAIL'] = 'test@example.com'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+  config.example_status_persistence_file_path = '/tmp/.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
